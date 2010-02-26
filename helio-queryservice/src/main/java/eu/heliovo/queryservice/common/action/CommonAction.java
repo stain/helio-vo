4e3057f7ca8e39537a34ec4878fd1510f88d3c09
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-02-26 15:10:13 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@503 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/common/action/CommonAction.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/common/action/CommonAction.java
@@ -1,23 +1,20 @@
-package eu.heliovo.queryservice.common.action;
+package com.org.helio.common.action;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
-
+import javax.servlet.http.HttpServletRequest;
 import com.opensymphony.xwork2.ActionSupport;
-
-import eu.heliovo.queryservice.common.dao.CommonDaoFactory;
-import eu.heliovo.queryservice.common.dao.interfaces.ShortNameQueryDao;
-import eu.heliovo.queryservice.common.transfer.CommonTO;
-import eu.heliovo.queryservice.common.transfer.FileResultTO;
-import eu.heliovo.queryservice.common.util.ConnectionManager;
-import eu.heliovo.queryservice.common.util.FileUtils;
-import eu.heliovo.queryservice.common.util.InstanceHolders;
+import com.org.helio.common.dao.CommonDaoFactory;
+import com.org.helio.common.dao.interfaces.ShortNameQueryDao;
+import com.org.helio.common.transfer.CommonTO;
+import com.org.helio.common.transfer.FileResultTO;
+import com.org.helio.common.util.ConnectionManager;
+import com.org.helio.common.util.FileUtils;
+import com.org.helio.common.util.InstanceHolders;
  
 public class CommonAction  extends ActionSupport
 {	
@@ -39,6 +36,7 @@ public class CommonAction  extends ActionSupport
 	{
 		return "SUCCESS";
 	}
+	
 	
 	public String display() throws SQLException{ 
 		String sReturnStatus="ERROR";
@@ -233,6 +231,7 @@ public class CommonAction  extends ActionSupport
 	private String jdbcUser;
 	private String jdbcPassword;
 	private String limitConstraint;
+	private String serviceDesc;
 	
 	public String[] getAddedTableDetails() {
 		return addedTableDetails;
@@ -300,11 +299,20 @@ public class CommonAction  extends ActionSupport
 	public void setLimitConstraint(String limitConstraint) {
 		this.limitConstraint = limitConstraint;
 	}
+	
+	public String getServiceDesc() {
+		return serviceDesc;
+	}
+
+	public void setServiceDesc(String serviceDesc) {
+		this.serviceDesc = serviceDesc;
+	}
 
 	public String createConfigurationFile()
 	{
 		String sReturnStatus="SUCCESS";
 		FileResultTO[] fileResultTO=null;
+		HttpServletRequest req = ServletActionContext.getRequest();
 		try{
 			
 			System.out.println(" :  Added Table Details  : "+getAddedTableDetails());
@@ -315,10 +323,11 @@ public class CommonAction  extends ActionSupport
 						String[] details=getAddedTableDetails()[i].split("\\^\\$\\$\\^");
 						System.out.println(getAddedTableDetails()[i]);
 						fileResultTO[i]=new FileResultTO();
-						fileResultTO[i].setJdbcDriverName("jdbc.driver="+jdbcDriverName);
-						fileResultTO[i].setJdbcUrl("jdbc.url="+jdbcUrl);
-						fileResultTO[i].setJdbcPassword("jdbc.password="+jdbcPassword);
-						fileResultTO[i].setJdbcUser("jdbc.user="+jdbcUser);
+						fileResultTO[i].setJdbcDriverName("jdbc.driver="+req.getParameter("jdbcDriverName"));
+						fileResultTO[i].setJdbcUrl("jdbc.url="+req.getParameter("jdbcUrl"));
+						fileResultTO[i].setJdbcPassword("jdbc.password="+req.getParameter("jdbcPassword"));
+						fileResultTO[i].setJdbcUser("jdbc.user="+req.getParameter("jdbcUser"));
+						fileResultTO[i].setServiceDesc("sql.votable.head.desc="+serviceDesc);
 						fileResultTO[i].setColumnNames("sql.columnnames."+details[0]+"="+details[1]);
 						fileResultTO[i].setColumnUCD("sql.columnucd."+details[0]+"="+details[8]);
 						fileResultTO[i].setColumnUType("sql.columnutypes."+details[0]+"=");
