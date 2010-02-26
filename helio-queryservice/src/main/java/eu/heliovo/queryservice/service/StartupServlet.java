dfe6250e191c3a7f80a4e27562bacc5da741d7d7
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-02-26 15:11:48 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@507 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/service/StartupServlet.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/service/StartupServlet.java
@@ -8,18 +8,30 @@ import com.org.helio.common.util.InstanceHolders;
 
 
 public class StartupServlet extends HttpServlet {
-
+ 
 	private static final long serialVersionUID = 1L;
 
 	public void init() throws ServletException {
 
 		try{
+			
+			/*
 			//Setting database connectivity while starting server.
 			InstanceHolders.getInstance().setProperty("jdbc.driver","org.apache.derby.jdbc.EmbeddedDriver");
-			InstanceHolders.getInstance().setProperty("jdbc.url","jdbc:derby:helio;create=true");
+			InstanceHolders.getInstance().setProperty("jdbc.url","jdbc:derby:/helio?schema=HELIO;create=true");
 			InstanceHolders.getInstance().setProperty("jdbc.user","helio");
 			InstanceHolders.getInstance().setProperty("jdbc.password","helio");
 			System.out.println("StartUp Servlet");
+			*/
+			
+			ClassLoader loader = this.getClass().getClassLoader();
+			// 
+			String sProfileFilePath=loader.getResource("test.txt").getFile();
+			if(sProfileFilePath!=null && !sProfileFilePath.equals("")){
+				InstanceHolders.getInstance().setProperty("hsqldb.database.path",sProfileFilePath.replaceAll("/test.txt", ""));
+			}
+			
+			System.out.println("++++++++++ Servleter started +++++++++++++");
 			
 		}
 		catch(Exception ex)
