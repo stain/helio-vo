6f541ea71602e2d885aa73c2db7f256f0e17592f
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-04-12 14:30:59 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@697 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/common/util/FileUtils.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/common/util/FileUtils.java
@@ -3,14 +3,46 @@ package eu.heliovo.queryservice.common.util;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedReader;
+import java.io.StringReader;
 import java.io.Writer;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import eu.heliovo.queryservice.common.transfer.FileResultTO;
 
 public class FileUtils {
 	
+	 protected final  Logger logger = Logger.getLogger(this.getClass());
 		
 	/**
 	  * Change the contents of text file in its entirety, overwriting any
@@ -120,6 +152,134 @@ public class FileUtils {
 
 
 	  
-	  
+	 private static Document readXml(StreamSource is) throws SAXException, IOException, ParserConfigurationException {
+
+	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+	    dbf.setValidating(false);
+	    dbf.setIgnoringComments(false);
+	    dbf.setIgnoringElementContentWhitespace(true);
+	    dbf.setNamespaceAware(true);
+	    // dbf.setCoalescing(true);
+	    // dbf.setExpandEntityReferences(true);
+
+	    DocumentBuilder db = null;
+	    db = dbf.newDocumentBuilder();
+	    db.setEntityResolver(new NullResolver());
+
+	    // db.setErrorHandler( new MyErrorHandler());
+	    InputSource is2 = new InputSource();
+	    is2.setSystemId(is.getSystemId());
+	    is2.setByteStream(is.getInputStream());
+	    is2.setCharacterStream(is.getReader());
+
+	    return db.parse(is2);
+	  }
+	   
+
+		// This method writes a DOM document to a file 
+	 public static void writeXmlFile(StreamSource is, String filename) throws IOException, ParserConfigurationException { 
+		 try { 
+			 // Prepare the DOM document for writing 
+			 Source source = new DOMSource(readXml(is)); 
+			 // Prepare the output file 
+			 File file = new File(filename); 
+			 Result result = new StreamResult(file);
+			 // Write the DOM document to the file 
+			 Transformer xformer = TransformerFactory.newInstance().newTransformer(); xformer.transform(source, result); 
+		 } catch (TransformerConfigurationException e) { 
+			System.out.println("  :   Exception occured while TransformerConfigurationException  : "+e.getMessage());
+		 } catch (TransformerException e) {
+			 System.out.println("  :   Exception occured while TransformerException  : "+e.getMessage());
+		 } catch (SAXException e) {
+			 System.out.println("  :   Exception occured while SAXException  : "+e.getMessage());
+		 }
+		 
+	   }    
+
+	 
+	 public static void createXmlFileFromInputStream(InputStream inputStream,String fileName)
+	 {
+		 try {
+			 File f=new File(fileName);
+			 OutputStream out=new FileOutputStream(f);
+		     byte buf[]=new byte[1024];
+		     int len;
+		     while((len=inputStream.read(buf))>0)
+		     out.write(buf,0,len);
+		     out.close();
+		     inputStream.close();
+		 }catch (Exception e) {
+	 			// System.err.println("error applying the xslt file "+e);
+	 			e.printStackTrace();
+	 		}
+	 }
+	 
+	 public static void writeStringIntoFile(String xmlData,String fileName)
+	 {
+	    try {
+	    	
+        	ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
+			out.writeObject(xmlData);
+			out.close();
+			out.flush();
+	    	/*XMLOutputFactory xof =  XMLOutputFactory.newInstance();
+	        XMLStreamWriter xtw = xof.createXMLStreamWriter(new FileWriter(fileName));
+	        xtw.writeCData(xmlData);
+	        xtw.flush();
+	        xtw.close();*/	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 	 }
+	 
+	 public static XMLStreamReader streamXmlDataFromFile(String fileName){
+		 try{
+			 
+			 URL url = Class.forName("FileUtils").getResource(fileName);            
+			 InputStream in = url.openStream();
+			 XMLInputFactory factory = XMLInputFactory.newInstance();
+			 XMLStreamReader parser = factory.createXMLStreamReader(in);
+			 
+			 return parser;
+		 }catch(Exception e){
+			 e.printStackTrace();
+		 }
+		return null;
+	 }
+	 
+  
+  @SuppressWarnings("unused")
+public static void exportToOoWriter(File file,PipedReader reader) throws Exception
+  {
+	  OutputStreamWriter zipout = new OutputStreamWriter(new FileOutputStream(file));
+ 		
+ 		Result result = new StreamResult(zipout);
+
+ 		// create an instance of TransformerFactory
+ 		try {
+ 			// System.out.println("make transform instance");
+ 			TransformerFactory transFact = TransformerFactory.newInstance();
+
+ 			Transformer trans = transFact.newTransformer(new StreamSource(reader));
+ 			trans.transform(new StreamSource(reader), result);
+ 		} catch (Exception e) {
+ 			// System.err.println("error applying the xslt file "+e);
+ 			e.printStackTrace();
+ 		}
+ 		
+ 		zipout.close();
+
+ 	}
+  
+  }
+  
+  
+  class NullResolver implements EntityResolver {
+	   public InputSource resolveEntity(String publicId, String systemId) throws SAXException,
+	       IOException {
+	     return new InputSource(new StringReader(""));
+	   }
 
 }
