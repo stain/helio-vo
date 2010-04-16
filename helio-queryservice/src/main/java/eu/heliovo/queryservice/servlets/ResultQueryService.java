301320b4a6ec19aa1004f0c8b702b831dddda377
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-04-16 15:34:36 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@743 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/servlets/ResultQueryService.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/servlets/ResultQueryService.java
@@ -15,12 +15,10 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Text;
-
 import eu.heliovo.queryservice.common.util.CommonUtils;
 import eu.heliovo.queryservice.common.util.FileUtils;
 import eu.heliovo.queryservice.common.util.HsqlDbUtils;
