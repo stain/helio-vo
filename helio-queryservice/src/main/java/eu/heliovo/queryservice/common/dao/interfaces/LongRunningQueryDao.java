3f8bda8269569de3d0ec223ade2fdb21545c548c
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-04-16 15:06:36 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@732 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/common/dao/interfaces/LongRunningQueryDao.java
@@ -0,0 +1,14 @@
+/* #ident	"%W%" */
+package eu.heliovo.queryservice.common.dao.interfaces;
+
+import eu.heliovo.queryservice.common.dao.exception.DetailsNotFoundException;
+import eu.heliovo.queryservice.common.transfer.criteriaTO.CommonCriteriaTO;
+
+ 
+public interface LongRunningQueryDao {
+	
+		public void insertStatusToHsqlDB(String randomUUIDString,String status) throws DetailsNotFoundException; 
+	    public void insertURLToHsqlDB(String randomUUIDString,String url) throws DetailsNotFoundException;
+		public String getStatusFromHsqlDB(String randomUUIDString) throws DetailsNotFoundException;
+		public String getUrlFromHsqlDB(String randomUUIDString) throws DetailsNotFoundException;
+}
