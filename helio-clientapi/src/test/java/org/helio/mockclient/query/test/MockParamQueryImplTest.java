8d22107482cfd6a04313ef588b84b136c41e59b2
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-06-01 14:26:07 +0000

    an inital test case
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@873 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-clientapi/src/test/java/org/helio/mockclient/query/test/MockParamQueryImplTest.java
@@ -0,0 +1,41 @@
+package org.helio.mockclient.query.test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import eu.heliovo.clientapi.query.HelioParameter;
+import eu.heliovo.mockclient.query.MockParamQueryImpl;
+
+
+public class MockParamQueryImplTest {
+
+	@Test
+	public void testDefaultQuery() {
+		MockParamQueryImpl mockParamQueryImpl = new MockParamQueryImpl();
+		HelioParameter[] params = mockParamQueryImpl.getParameterDescription(null);
+		System.out.println(Arrays.toString(params));
+		Assert.assertEquals(1, params.length);
+		
+		HelioParameter param = params[0];
+		Assert.assertEquals("service", param.getParamName());
+		Assert.assertEquals("Available query parameter services.", param.getDescription());
+		Assert.assertEquals("xsd:string", param.getXsdType());		
+	}
+	
+	@Test
+	public void testHecQuery() {
+		MockParamQueryImpl mockParamQueryImpl = new MockParamQueryImpl();
+		HelioParameter[] params = mockParamQueryImpl.getParameterDescription(Collections.singletonMap("service", "hec"));
+		System.out.println(Arrays.toString(params));
+		Assert.assertEquals(3, params.length);
+		
+		HelioParameter param = params[0];
+		Assert.assertEquals("startdate", param.getParamName());
+		Assert.assertEquals("the start date", param.getDescription());
+		Assert.assertEquals("xsd:dateTime", param.getXsdType());		
+		
+	}
+}
