7e1b0052c876b76b15ba7446295644129de9e6fc
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-06-17 18:56:05 +0000

    add model root
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@936 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-clientapi/src/main/java/eu/heliovo/clientapi/model/HelioRoot.java
@@ -0,0 +1,11 @@
+package eu.heliovo.clientapi.model;
+
+/**
+ * Root object of the Helio model.
+ * TOOD: document.  
+ * @author marco soldati at fhnw ch
+ *
+ */
+public interface HelioRoot {
+
+}
