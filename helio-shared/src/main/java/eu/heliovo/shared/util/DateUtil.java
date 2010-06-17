ea864d5f7713e649adeefb7d8ba20370b97ecdbb
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-06-17 19:01:17 +0000

    add date util
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@940 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-shared/src/main/java/eu/heliovo/shared/util/DateUtil.java
@@ -0,0 +1,39 @@
+package eu.heliovo.shared.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Utilities to handle Java Dates. 
+ * @author marco soldati at fhnw ch
+ *
+ */
+public class DateUtil {
+
+	/**
+	 * Formatter for the ISO format.
+	 */
+	private static DateFormat ISO8601_FORMAT = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss");
+
+	/**
+	 * Convert a date object to an ISO date string such as used by XML docs.
+	 * 
+	 * @param date
+	 *            the date to convert.
+	 * @return the date as string.
+	 */
+	public static String toIsoDateString(Date date) {
+		if (date == null) {
+			return null;
+		}
+
+		// format in (almost) ISO8601 format
+		String dateStr = ISO8601_FORMAT.format(date);
+
+		// remap the timezone from 0000 to 00:00 (starts at char 22)
+		// return dateStr.substring (0, 22) + ":" + dateStr.substring (22);
+		return dateStr;
+	};
+}
---
+++ helio-shared/src/test/java/eu/heliovo/shared/util/DateUtilTest.java
@@ -0,0 +1,30 @@
+package eu.heliovo.shared.util;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+/**
+ * Unit test for the date util
+ * @author marco soldati at fhnw ch
+ *
+ */
+public class DateUtilTest {
+	
+	@Test
+	public void testToIsoDateString() {
+		Date date = new Date(1104534000328l); // "2005-01-01T00:00:00
+		assertEquals("2005-01-01T00:00:00", DateUtil.toIsoDateString(date));
+		
+		date = new Date(0);
+		
+		// TODO: This should be 1970-01-01T00:00:00
+		assertEquals("1970-01-01T01:00:00", DateUtil.toIsoDateString(date));
+		
+		
+		
+		
+	}
+}
