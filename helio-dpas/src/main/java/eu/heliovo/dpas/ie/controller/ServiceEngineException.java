706fdf124a4d70019c1a7ade5e8a010eb5695f7b
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-16 10:38:54 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1022 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/controller/ServiceEngineException.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/controller/ServiceEngineException.java
@@ -6,4 +6,27 @@ package eu.heliovo.dpas.ie.controller;
 public class ServiceEngineException extends Exception 
 {
 	private static final long serialVersionUID = -5653162248347058351L;
+
+	String error="";
+	public ServiceEngineException()
+	{
+		super();             
+		error = "unknown";
+	}
+
+	public ServiceEngineException(String err)
+	{
+		super(err);   
+		error = err;  
+	}
+	public ServiceEngineException(String err,Throwable tr)
+	{
+		super(err,tr);   
+		error = err;  
+	}	
+
+	public String getError()
+	{
+		return error;
+	}
 }
