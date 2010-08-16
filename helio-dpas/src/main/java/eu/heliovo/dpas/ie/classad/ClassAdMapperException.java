855f11d1ca72bacf22b89c0c96123b0af2d70e47
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-16 10:43:50 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1024 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/classad/ClassAdMapperException.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/classad/ClassAdMapperException.java
@@ -9,5 +9,28 @@ package eu.heliovo.dpas.ie.classad;
  */
 public class ClassAdMapperException extends Exception
 {
-	private static final long serialVersionUID = 6274998306701919994L;
+	private static final long serialVersionUID = -5653162248347058351L;
+
+	String error="";
+	public ClassAdMapperException()
+	{
+		super();             
+		error = "unknown";
+	}
+
+	public ClassAdMapperException(String err)
+	{
+		super(err);   
+		error = err;  
+	}
+	public ClassAdMapperException(String err,Throwable tr)
+	{
+		super(err,tr);   
+		error = err;  
+	}	
+
+	public String getError()
+	{
+		return error;
+	}
 }
