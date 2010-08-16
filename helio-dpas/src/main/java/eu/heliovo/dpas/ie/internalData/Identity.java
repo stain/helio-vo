7b2bb93d717f1e691a5ef28bc666a054ad1771e8
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-16 10:54:12 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1030 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/internalData/Identity.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/internalData/Identity.java
@@ -3,4 +3,7 @@ package eu.heliovo.dpas.ie.internalData;
 public class Identity
 {
 
+	/**
+	 * Not sure why this class is empty; Deleting this class would infact many clasess and methods; THis will be solved in Phase 2 of (dpas engineering).
+	 */
 }
