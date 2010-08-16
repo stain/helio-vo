9d00188f410404d8ac7a1b8ce26b012abb4321a2
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-16 11:06:00 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1035 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/sensors/archives/GenericPathFragmentException.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/sensors/archives/GenericPathFragmentException.java
@@ -4,5 +4,28 @@ package eu.heliovo.dpas.ie.sensors.archives;
 public class GenericPathFragmentException extends Exception
 {
 	private static final long serialVersionUID = 5237896903254923902L;
+	// added vy Vineeth
+	String error="";
+	public GenericPathFragmentException()
+	{
+		super();             
+		error = "unknown";
+	}
+
+	public GenericPathFragmentException(String err)
+	{
+		super(err);   
+		error = err;  
+	}
+	public GenericPathFragmentException(String err,Throwable tr)
+	{
+		super(err,tr);   
+		error = err;  
+	}	
+
+	public String getError()
+	{
+		return error;
+	}
 
 }
