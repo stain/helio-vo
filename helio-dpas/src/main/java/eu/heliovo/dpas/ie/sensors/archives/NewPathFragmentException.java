2a5f726f6b7ae2fe703601b801767d88e03e8c60
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-16 11:04:56 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1034 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/sensors/archives/NewPathFragmentException.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/sensors/archives/NewPathFragmentException.java
@@ -4,4 +4,28 @@ package eu.heliovo.dpas.ie.sensors.archives;
 public class NewPathFragmentException extends Exception
 {
 	private static final long serialVersionUID = -8514728056228954311L;
+	
+	// added vy Vineeth
+	String error="";
+	public NewPathFragmentException()
+	{
+		super();             
+		error = "unknown";
+	}
+
+	public NewPathFragmentException(String err)
+	{
+		super(err);   
+		error = err;  
+	}
+	public NewPathFragmentException(String err,Throwable tr)
+	{
+		super(err,tr);   
+		error = err;  
+	}	
+
+	public String getError()
+	{
+		return error;
+	}
 }
