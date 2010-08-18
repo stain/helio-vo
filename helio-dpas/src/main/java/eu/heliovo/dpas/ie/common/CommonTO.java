b8c3c94732bab280c3feff7def779772ddec693c
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-18 10:38:39 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1044 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/common/CommonTO.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/common/CommonTO.java
@@ -1,6 +1,7 @@
 package eu.heliovo.dpas.ie.common;
 
 import java.io.Writer;
+import java.util.Calendar;
 
 public class CommonTO {
 
@@ -13,6 +14,45 @@ public class CommonTO {
 	private int[] dataLevels;
 	private Writer printWriter;
 	private boolean votable;
+	private String instrument;
+	private Calendar dateFrom;
+	private Calendar dateTo;
+	private  String url;
+	
+	private int maxResults;
+	
+	public String getInstrument() {
+		return instrument;
+	}
+	public void setInstrument(String instrument) {
+		this.instrument = instrument;
+	}
+	public Calendar getDateFrom() {
+		return dateFrom;
+	}
+	public void setDateFrom(Calendar dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+	public Calendar getDateTo() {
+		return dateTo;
+	}
+	public void setDateTo(Calendar dateTo) {
+		this.dateTo = dateTo;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public int getMaxResults() {
+		return maxResults;
+	}
+	public void setMaxResults(int maxResults) {
+		this.maxResults = maxResults;
+	}
+	
 	
 	public String[] getInstruments() {
 		return instruments;
