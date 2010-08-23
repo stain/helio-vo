d6252e498b2a0862fc9c5aa2d9185cfb6df650ed
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-23 10:27:56 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1057 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/DpasQueryServlet.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/DpasQueryServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import eu.heliovo.dpas.ie.common.DAOFactory;
 import eu.heliovo.dpas.ie.services.vso.transfer.VsoDataTO;
+import eu.heliovo.dpas.ie.services.vso.utils.VsoUtils;
 
 /**
  * Servlet implementation class DpasQueryServlet
@@ -31,13 +33,36 @@ public class DpasQueryServlet extends HttpServlet {
 		response.setContentType("text/xml;charset=UTF-8");
 		VsoDataTO vsoTO=new VsoDataTO();
 		PrintWriter pw = response.getWriter(); 
-		
+		String instruments[]=null;
+		String startTime[]=null;
+		String stopTime[]=null;
 		try{
-		    //Setting start time & end time parameter
-		    String sStartTime=request.getParameter("STARTTIME");
-		    String sEndTime=request.getParameter("ENDTIME");			
-		    //Setting for Instrument parameter.
-		    String sInstrument=request.getParameter("INSTRUMENT");
+		     //Setting start time & end time parameter
+		     String sStartTime=request.getParameter("STARTTIME");
+		     String sEndTime=request.getParameter("ENDTIME");			
+		     //Setting for Instrument parameter.
+		     String sInstrument=request.getParameter("INSTRUMENT");
+		     //Start time
+		     if(sStartTime!=null)
+		    	 startTime=sStartTime.split(",");
+		     //End Time
+		     if(sStartTime!=null)
+		    	 stopTime=sStartTime.split(",");
+		     //Instrument
+		     if(sInstrument!=null)
+		    	 instruments=sInstrument.split(",");
+		     vsoTO.setUrl(VsoUtils.getUrl(request));
+		     vsoTO.setInstruments(instruments);
+		     vsoTO.setStartTimes(startTime);
+		     vsoTO.setStopTimes(stopTime);
+		     vsoTO.setOutput(pw);
+		     vsoTO.setWhichProvider("VSO");
+		     vsoTO.setStatus("webservice");
+		     vsoTO.setVotableDescription("VSO query response");
+		     //responseReader= queryService.sortedQuery(instruments, startTime, stopTime, false, null, null,votable);
+		     //Calling DAO factory to connect VSO
+		     DAOFactory daoFactory= DAOFactory.getDAOFactory(vsoTO.getWhichProvider());
+		     daoFactory.getVsoQueryDao().query(vsoTO);
 		    
 		}catch(Exception e){
 			e.printStackTrace();
