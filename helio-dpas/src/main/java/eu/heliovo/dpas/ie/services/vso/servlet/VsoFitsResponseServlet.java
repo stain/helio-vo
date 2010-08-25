d0303ad62861f1cc0b882b104d38f7e044039685
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-08-25 10:26:30 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1067 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/vso/servlet/VsoFitsResponseServlet.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/vso/servlet/VsoFitsResponseServlet.java
@@ -1,10 +1,15 @@
 package eu.heliovo.dpas.ie.services.vso.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import eu.heliovo.dpas.ie.common.DAOFactory;
+import eu.heliovo.dpas.ie.services.vso.transfer.VsoDataTO;
 
 /**
  * Servlet implementation class VsoFitsResponseServlet
@@ -25,6 +30,34 @@ public class VsoFitsResponseServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("application/octet-stream");
+		VsoDataTO vsoTO=new VsoDataTO();
+		PrintWriter pw = response.getWriter();
+		
+		try{
+			//Setting start time & end time parameter
+		     String fileId=request.getParameter("ID");
+		     String provider=request.getParameter("PROVIDER");			
+		     //Setting for Instrument parameter.
+		     vsoTO.setOutput(pw);
+		     vsoTO.setWhichProvider("VSO");
+		     vsoTO.setStatus("webservice");
+		     vsoTO.setProvider(provider);
+		     vsoTO.setFileId(fileId);
+		     //Calling DAO factory to connect VSO
+		     DAOFactory daoFactory= DAOFactory.getDAOFactory(vsoTO.getWhichProvider());
+		     daoFactory.getVsoQueryDao().getFitsFile(vsoTO);
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println(" : Exception occured while creating the file :  "+e.getMessage());
+		}		
+		finally
+		{
+			if(pw!=null){
+				pw.close();
+				pw=null;
+			}
+		}
 	}
 
 	/**
@@ -32,6 +65,7 @@ public class VsoFitsResponseServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
 	}
 
 }
