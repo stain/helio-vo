284f16488779cf63d328fad9e6ba145d7f42bf23
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-15 16:00:16 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1104 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/common/DAOFactory.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/common/DAOFactory.java
@@ -1,18 +1,19 @@
 package eu.heliovo.dpas.ie.common;
 
-import eu.heliovo.dpas.ie.services.vso.dao.CommonDaoFactory;
+import eu.heliovo.dpas.ie.dataProviders.DPASDataProvider;
+import eu.heliovo.dpas.ie.services.CommonDaoFactory;
 import eu.heliovo.dpas.ie.services.vso.dao.interfaces.VsoQueryDao;
 
 public abstract class DAOFactory {
 	
 	  public abstract VsoQueryDao getVsoQueryDao();
 	
-	  public static DAOFactory getDAOFactory(
+	  public static DPASDataProvider getDAOFactory(
 	    String whichFactory) {
 		Type type=Type.valueOf(whichFactory);
 	    switch (type) {
 	      case VSO: 
-	          return CommonDaoFactory.getInstance();
+	          return CommonDaoFactory.getInstance().getVsoQueryDao();
 	      case CDAWEB    : 
 	          return null;      
 	      case DIR    : 
