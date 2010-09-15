1b13bc481adea0be2f0c839a4b1e76b19d4005e4
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-15 14:25:19 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1095 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/transfer/UocDataTO.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/transfer/UocDataTO.java
@@ -1,44 +1,8 @@
 package eu.heliovo.dpas.ie.services.uoc.transfer;
 
-import java.io.Writer;
 import eu.heliovo.dpas.ie.common.CommonTO;
 
 public class UocDataTO extends CommonTO{
 
-	private Writer output;
-	private boolean providerStatus;	
-	private String fileId;
-	private String provider;
 	
-	public Writer getOutput() {
-		return output;
-	}
-
-	public void setOutput(Writer output) {
-		this.output = output;
-	}
-
-	public boolean isProviderStatus() {
-		return providerStatus;
-	}
-
-	public void setProviderStatus(boolean providerStatus) {
-		this.providerStatus = providerStatus;
-	}
-
-	public String getFileId() {
-		return fileId;
-	}
-
-	public void setFileId(String fileId) {
-		this.fileId = fileId;
-	}
-
-	public String getProvider() {
-		return provider;
-	}
-
-	public void setProvider(String provider) {
-		this.provider = provider;
-	}
 }
