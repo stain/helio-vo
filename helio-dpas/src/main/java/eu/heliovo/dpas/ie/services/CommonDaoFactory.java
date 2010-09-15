fc269cbc28804ba248f2e531ffda7c1ee68fd9e0
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-15 14:27:22 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1098 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/CommonDaoFactory.java
@@ -0,0 +1,27 @@
+/* #ident	"%W%" */
+package eu.heliovo.dpas.ie.services;
+
+import eu.heliovo.dpas.ie.common.DAOFactory;
+import eu.heliovo.dpas.ie.services.vso.dao.impl.VsoQueryDaoImpl;
+import eu.heliovo.dpas.ie.services.vso.dao.interfaces.VsoQueryDao;
+
+public class CommonDaoFactory extends DAOFactory {
+
+	private static CommonDaoFactory instance = null;
+	
+	private CommonDaoFactory(){
+		//Constructor
+	}
+
+	public static CommonDaoFactory getInstance(){
+		if(instance==null){
+			instance = new CommonDaoFactory();
+		}
+		return instance;
+	}
+
+	public VsoQueryDao getVsoQueryDao(){
+		return new VsoQueryDaoImpl();
+	}
+	
+}
