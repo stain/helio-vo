54ade2204752a5b25fee1d7692b9685d8af8b221
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-15 14:25:31 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1096 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/utils/QueryThreadAnalizer.java
@@ -0,0 +1,28 @@
+package eu.heliovo.dpas.ie.services.uoc.utils;
+
+import java.io.BufferedWriter;
+
+import uk.ac.starlink.votable.VOTableWriter;
+import eu.heliovo.dpas.ie.common.DAOFactory;
+import eu.heliovo.dpas.ie.common.VOTableCreator;
+import eu.heliovo.dpas.ie.services.vso.transfer.VsoDataTO;
+
+public class QueryThreadAnalizer extends Thread
+{	
+	
+	private VsoDataTO vsoDatTO=null;
+	public QueryThreadAnalizer(VsoDataTO vsoTO){
+		vsoDatTO=vsoTO;
+	}
+	public void run(){
+		  
+		BufferedWriter out =null;
+		try{
+			  DAOFactory daoFactory= DAOFactory.getDAOFactory(vsoDatTO.getWhichProvider());
+			  daoFactory.getVsoQueryDao().generateVOTable(vsoDatTO);
+		}catch(Exception pe) {			
+			System.out.println("  : Exception : "+pe);
+		}
+	 }
+	
+}
