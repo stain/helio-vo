f8b345be95410774247c2f868c33809bd73bcb57
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-16 11:19:03 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1112 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/vso/dao/interfaces/VsoQueryDao.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/vso/dao/interfaces/VsoQueryDao.java
@@ -4,6 +4,7 @@ package eu.heliovo.dpas.ie.services.vso.dao.interfaces;
 import java.io.Writer;
 import java.util.Calendar;
 
+import eu.heliovo.dpas.ie.common.CommonTO;
 import eu.heliovo.dpas.ie.dataProviders.DPASDataProvider;
 import eu.heliovo.dpas.ie.services.vso.dao.exception.DataNotFoundException;
 import eu.heliovo.dpas.ie.services.vso.transfer.VsoDataTO;
@@ -12,9 +13,8 @@ import eu.heliovo.dpas.ie.services.vso.transfer.VsoDataTO;
 public interface VsoQueryDao extends DPASDataProvider {
 
 	//public	void query(String instrument,  String dateFrom, String dateTo, int maxResults,String url,Writer output,String providerName) throws Exception;
-	public void query(VsoDataTO vsoTO) throws Exception;
+	public void query(CommonTO commonTO) throws Exception;
 	public void generateVOTable(VsoDataTO vsoTO) throws DataNotFoundException,Exception;
 	public void getFitsFile(VsoDataTO vsoTO) throws Exception;
-	
 
 }
