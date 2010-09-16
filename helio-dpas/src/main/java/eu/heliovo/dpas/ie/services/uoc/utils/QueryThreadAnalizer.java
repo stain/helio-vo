ebf4b4300b414f84aaaaaa14d0eed6992d9452a5
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-16 13:46:40 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1120 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/utils/QueryThreadAnalizer.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/utils/QueryThreadAnalizer.java
@@ -5,21 +5,24 @@ import java.io.BufferedWriter;
 import uk.ac.starlink.votable.VOTableWriter;
 import eu.heliovo.dpas.ie.common.DAOFactory;
 import eu.heliovo.dpas.ie.common.VOTableCreator;
+import eu.heliovo.dpas.ie.services.uoc.dao.interfaces.UocQueryDao;
+import eu.heliovo.dpas.ie.services.uoc.transfer.UocDataTO;
+import eu.heliovo.dpas.ie.services.vso.dao.interfaces.VsoQueryDao;
 import eu.heliovo.dpas.ie.services.vso.transfer.VsoDataTO;
 
 public class QueryThreadAnalizer extends Thread
 {	
 	
-	private VsoDataTO vsoDatTO=null;
-	public QueryThreadAnalizer(VsoDataTO vsoTO){
-		vsoDatTO=vsoTO;
+	private UocDataTO uocTO=null;
+	public QueryThreadAnalizer(UocDataTO uocTO){
+		uocTO=uocTO;
 	}
 	public void run(){
 		  
 		BufferedWriter out =null;
 		try{
-			  //DAOFactory daoFactory= DAOFactory.getDAOFactory(vsoDatTO.getWhichProvider());
-			  //daoFactory.getVsoQueryDao().generateVOTable(vsoDatTO);
+			UocQueryDao uocQueryDao= (UocQueryDao) DAOFactory.getDAOFactory(uocTO.getWhichProvider());
+			uocQueryDao.generateVOTable(uocTO);
 		}catch(Exception pe) {			
 			System.out.println("  : Exception : "+pe);
 		}
