46c01c54177b0fd7e3f20a4dcc1a628e6eddbd82
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-17 10:05:12 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1130 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/dao/interfaces/UocQueryDao.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/dao/interfaces/UocQueryDao.java
@@ -1,16 +1,14 @@
 /* #ident	"%W%" */
 package eu.heliovo.dpas.ie.services.uoc.dao.interfaces;
 
+import eu.heliovo.dpas.ie.common.CommonTO;
 import eu.heliovo.dpas.ie.dataProviders.DPASDataProvider;
 import eu.heliovo.dpas.ie.services.uoc.dao.exception.DataNotFoundException;
 import eu.heliovo.dpas.ie.services.uoc.transfer.UocDataTO;
 
 public interface UocQueryDao extends DPASDataProvider {
 
-	void query(UocDataTO uocTO) throws Exception;
+	void query(CommonTO commonTO) throws Exception;
 
-	void generateVOTable(UocDataTO vsoTO) throws DataNotFoundException,
-			Exception;
-	
-
+	void generateVOTable(UocDataTO vsoTO) throws DataNotFoundException,Exception;
 }
