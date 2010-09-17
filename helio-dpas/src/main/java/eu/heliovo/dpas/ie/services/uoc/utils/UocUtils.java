e78b7caabbfa87e6314e1b46a3ca4321987e314f
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-17 10:02:46 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1122 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/utils/UocUtils.java
@@ -0,0 +1,45 @@
+/* #ident	"%W%" */
+package eu.heliovo.dpas.ie.services.uoc.utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
+import eu.heliovo.dpas.ie.common.ConstantKeywords;
+import eu.heliovo.dpas.ie.services.vso.service.org.virtualsolar.VSO.VSOi.ProviderQueryResponse;
+
+
+public class UocUtils {
+	
+	 protected final  Logger logger = Logger.getLogger(this.getClass());
+	
+		  
+	  /**
+	   * 
+	   * @param date
+	   * @return
+	   */
+	  public static String getDateFormat(String date){
+		
+		 try
+          {
+			SimpleDateFormat sdf = new SimpleDateFormat(ConstantKeywords.ORGINALDATEFORMAT.getDateFormat());
+			Date dt=sdf.parse(date.replace("T", " "));
+            //Converting back the format
+            DateFormat dateFormat = new SimpleDateFormat(ConstantKeywords.VSODATEFORMAT.getDateFormat());
+   	     	dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));	
+   	     	String datetime=dateFormat.format(dt);
+   	     	System.out.println(" Date and time "+datetime);
+  	     	return datetime;
+         } catch (Exception e)
+		 {
+		    e.printStackTrace();
+		 }
+		  return null;
+	  }
+	  
+
+				
+}
