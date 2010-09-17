bd9b4068be282921da6a1cd000187cf4604c3d16
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-17 10:04:18 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1127 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/transfer/UocDataTO.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/transfer/UocDataTO.java
@@ -1,8 +1,60 @@
 package eu.heliovo.dpas.ie.services.uoc.transfer;
 
+import java.io.Writer;
+
 import eu.heliovo.dpas.ie.common.CommonTO;
+import eu.heliovo.dpas.ie.services.uoc.service.net.ivoa.xml.votable.v1.RESOURCE;
 
 public class UocDataTO extends CommonTO{
 
+	private boolean providerStatus;	
+	private String fileId;
+	private String provider;
+	
+	private Writer output;
+	private RESOURCE resource;
+	
+	
+	public void setOutput(Writer output) {
+		this.output = output;
+	}
+
+	public Writer getOutput() {
+		return output;
+	}
+
+	public boolean isProviderStatus() {
+		return providerStatus;
+	}
+
+	public void setProviderStatus(boolean providerStatus) {
+		this.providerStatus = providerStatus;
+	}
+
+	public String getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public RESOURCE getResource() {
+		return resource;
+	}
+
+	public void setResource(RESOURCE resource) {
+		this.resource = resource;
+	}
+
+	
 	
 }
