54be9cff0844cd92c9824fbdef3ac4bdb452893a
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-17 15:06:08 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1140 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/common/DAOFactory.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/common/DAOFactory.java
@@ -2,12 +2,16 @@ package eu.heliovo.dpas.ie.common;
 
 import eu.heliovo.dpas.ie.dataProviders.DPASDataProvider;
 import eu.heliovo.dpas.ie.services.CommonDaoFactory;
+import eu.heliovo.dpas.ie.services.cdaweb.dao.interfaces.CdaWebQueryDao;
+import eu.heliovo.dpas.ie.services.uoc.dao.interfaces.UocQueryDao;
 import eu.heliovo.dpas.ie.services.vso.dao.interfaces.VsoQueryDao;
 
 public abstract class DAOFactory {
 	
 	  public abstract VsoQueryDao getVsoQueryDao();
-	
+	  public abstract UocQueryDao getUocQueryDao();
+	  public abstract CdaWebQueryDao getCdaWebQueryDao();
+	  
 	  public static DPASDataProvider getDAOFactory(
 	    String whichFactory) {
 		Type type=Type.valueOf(whichFactory);
@@ -15,18 +19,18 @@ public abstract class DAOFactory {
 	      case VSO: 
 	          return (VsoQueryDao)CommonDaoFactory.getInstance().getVsoQueryDao();
 	      case CDAWEB    : 
-	          return null;      
+	          return (CdaWebQueryDao)CommonDaoFactory.getInstance().getCdaWebQueryDao();      
 	      case DIR    : 
 	          return null;
-	     
+	      case UOC		:
+	    	  return (UocQueryDao)CommonDaoFactory.getInstance().getUocQueryDao();
 	      default        : 
 	          return null;
 	    }
 	  }
-	  
-	  
+	   	  
 	  enum Type{
-		  VSO,CDAWEB,DIR
+		  VSO,CDAWEB,DIR,UOC
 		};
 
 
