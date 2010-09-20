b3d93774bbeedeecbe2dfa587fb7e2c45a2ebcf0
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-20 13:31:24 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1147 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/cdaweb/provider/CDAWEBProvider.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/cdaweb/provider/CDAWEBProvider.java
@@ -1,31 +1,35 @@
 package eu.heliovo.dpas.ie.services.cdaweb.provider;
 
-
 import java.net.URL;
 import java.util.Calendar;
-
+import eu.heliovo.dpas.ie.common.DAOFactory;
 import eu.heliovo.dpas.ie.services.cdaweb.dao.exception.DataNotFoundException;
+import eu.heliovo.dpas.ie.services.cdaweb.dao.interfaces.CdaWebQueryDao;
 import eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.DatasetDescription;
 import eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.FileDescription;
 import eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.ViewDescription;
 import eu.heliovo.dpas.ie.services.cdaweb.transfer.CdaWebDataTO;
 import eu.heliovo.dpas.ie.services.cdaweb.utils.CdaWebUtils;
 import uk.ac.starlink.table.StarTable;
-
-
+import eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CoordinatedDataAnalysisSystemBindingStub;
+import eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CDASWSLocator;
+import eu.heliovo.dpas.ie.services.cdaweb.utils.PointsStarTable;
 
 public class CDAWEBProvider
 {
 
 	public	void query(CdaWebDataTO cdaWebTO) throws DataNotFoundException {
 		
-		StarTable[] tables=null;
-		eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CoordinatedDataAnalysisSystemBindingStub binding;
+		StarTable[] tables=new StarTable[1];
+		CoordinatedDataAnalysisSystemBindingStub binding;
+
         try{
-        	binding = (eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CoordinatedDataAnalysisSystemBindingStub)
-            new eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CDASWSLocator().getCoordinatedDataAnalysisSystemPort();
+        	binding = (CoordinatedDataAnalysisSystemBindingStub)
+            new CDASWSLocator().getCoordinatedDataAnalysisSystemPort();
         	String []instr = new String[1];
         	instr[0]=cdaWebTO.getInstrument();
+        	String [] missionName = new String[1];
+        	missionName[0]="Ulysses";
         	ViewDescription[] value = null;        
             ViewDescription[] views = binding.getAllViewDescriptions();
             //Start Date
@@ -36,32 +40,43 @@ public class CDAWEBProvider
             Calendar startTemp = Calendar.getInstance();
             //Temp end time
             Calendar endTemp = Calendar.getInstance();
-            
+            //
+            FileDescription[] fileDesc=new FileDescription[0] ;
             for(int i = 0;i < views.length;i++) {
                 if(views[i].isPublicAccess() && views[i].getId().equals("sp_phys")) {
-                       binding = (eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CoordinatedDataAnalysisSystemBindingStub)
-                                     new eu.heliovo.dpas.ie.services.cdaweb.service.org.ws.cdaw.CDASWSLocator().getCoordinatedDataAnalysisSystemPort(new URL(views[i].getEndpointAddress()));
-                       
-                       DatasetDescription[] dsd = binding.getDatasetsByInstrument(instr, new String[0]);
+                       binding = (CoordinatedDataAnalysisSystemBindingStub)new CDASWSLocator().getCoordinatedDataAnalysisSystemPort(new URL(views[i].getEndpointAddress()));
+                       DatasetDescription[] dsd = binding.getDatasetsByInstrument(instr, new String[1]);
+                       DatasetDescription[] dsd1 = binding.getDatasets(missionName, new String[1]);
                        //noDuplMap.clear();
-                       for(int k = 0;k < dsd.length;k++) {
-                          if(startTime.before(dsd[k].getStartTime())) {
+                       //tables=new StarTable[dsd1.length];
+                       for(int k = 0;k < dsd1.length;k++) {
+                          if(startTime.before(dsd1[k].getStartTime())) {
                               System.out.println("WARNING: Your given start time is before the dataset start time; using dataset start time");
-                              startTemp = dsd[k].getStartTime();
+                              startTemp = dsd1[k].getStartTime();
                           }else {
                               startTemp = startTime;
                           }
-                          if(endTime.after(dsd[k].getEndTime())) {
+                          if(endTime.after(dsd1[k].getEndTime())) {
                               System.out.println("WARNING: Your given end time is after the datasets end time; using the dataset end time");
-                              endTemp = dsd[k].getEndTime();
+                              endTemp = dsd1[k].getEndTime();
                           }else {
                               endTemp = endTime;
                           }
-                          FileDescription[] fds = binding.getDataFiles(dsd[k].getId(),startTemp,endTemp);
+                          FileDescription[] fds = binding.getDataFiles(dsd1[k].getId(),startTemp,endTemp);
+                          fileDesc=CdaWebUtils.addArrays(fileDesc, fds);
+                          System.out.println("PI Name "+dsd1[k].getPiName());
+                           System.out.println("....DONE !!!");
                        }//for
+                       tables[0]=new PointsStarTable(fileDesc,cdaWebTO.getInstrument());
+                       cdaWebTO.setStarTableArray(tables);
+                       cdaWebTO.setQuerystatus("OK");
+       		           //Call to CDAWEB dao
+                       CdaWebQueryDao cdaWebQueryDao=(CdaWebQueryDao) DAOFactory.getDAOFactory(cdaWebTO.getWhichProvider());
+                       cdaWebQueryDao.generateVOTable(cdaWebTO);
                 }
             }
         }catch(Exception e){
+        	e.printStackTrace();
         	throw new DataNotFoundException(" Could not retrieve data: ",e);
         }
 
