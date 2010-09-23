7f7c11ef24f9ab062563f286ed76bd1ae1d00a07
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-23 12:30:16 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1162 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/action/FileUploadAction.java
@@ -0,0 +1,70 @@
+package eu.heliovo.dpas.ie.services.common.action;
+
+import java.io.File;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import com.opensymphony.xwork2.ActionSupport;
+
+import eu.heliovo.dpas.ie.services.common.utils.InstanceHolders;
+
+public class FileUploadAction extends ActionSupport implements
+		ServletRequestAware {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private File userFile;
+	private String userFileContentType;
+	private String userFileFileName;
+
+	private HttpServletRequest servletRequest;
+
+	public String execute() {
+		try {
+			//File name tmp file path
+			System.out.println(" File name "+ this.userFileFileName+" User File "+this.userFile);
+			String hsqlFilePath=InstanceHolders.getInstance().getProperty("hsqldb.database.path")+"/HelioDB/";
+			System.out.println(" : Provider access table path : "+userFile+" : Hsql data path : "+hsqlFilePath);
+			File fileToCreate = new File(hsqlFilePath, this.userFileFileName);
+			//Copying file HSQL database.
+			FileUtils.copyFile(this.userFile, fileToCreate);
+		} catch (Exception e) {
+			e.printStackTrace();
+			addActionError(e.getMessage());
+
+			return INPUT;
+		}
+		return SUCCESS;
+	}
+
+	public File getUserFile() {
+		return userFile;
+	}
+
+	public void setUserFile(File userFile) {
+		this.userFile = userFile;
+	}
+
+	public String getUserFileContentType() {
+		return userFileContentType;
+	}
+
+	public void setUserFileContentType(String userFileContentType) {
+		this.userFileContentType = userFileContentType;
+	}
+
+	public String getUserFileFileName() {
+		return userFileFileName;
+	}
+
+	public void setUserFileFileName(String userFileFileName) {
+		this.userFileFileName = userFileFileName;
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest servletRequest) {
+		this.servletRequest = servletRequest;
+
+	}
+}
