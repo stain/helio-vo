850cf769816b388f860dc2f44496470f979464ed
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-23 14:49:55 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1171 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/common/util/CommonUtils.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/common/util/CommonUtils.java
@@ -8,17 +8,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
-
 import eu.heliovo.queryservice.common.transfer.FileResultTO;
 
 
