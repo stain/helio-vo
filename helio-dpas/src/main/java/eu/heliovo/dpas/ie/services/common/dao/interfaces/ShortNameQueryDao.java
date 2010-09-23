44d36c3c906d122b4895909b6d9833f9dfee13e1
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-23 12:29:38 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1160 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/interfaces/ShortNameQueryDao.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/interfaces/ShortNameQueryDao.java
@@ -4,8 +4,10 @@ package eu.heliovo.dpas.ie.services.common.dao.interfaces;
 import java.sql.Connection;
 import java.util.HashMap;
 
+import eu.heliovo.dpas.ie.services.common.dao.exception.DetailsNotFoundException;
+
 
 public interface ShortNameQueryDao {
 
-	
+	public void loadProviderAccessTable(String fileName) throws DetailsNotFoundException;
 }
