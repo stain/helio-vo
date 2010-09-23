32995f874433d2edfb83806b86702e649ca20612
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-23 12:30:31 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1163 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/action/CommonAction.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/action/CommonAction.java
@@ -1,30 +1,27 @@
 package eu.heliovo.dpas.ie.services.common.action;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
 import com.opensymphony.xwork2.ActionSupport;
+
+import eu.heliovo.dpas.ie.services.common.utils.CommonUtils;
+import eu.heliovo.dpas.ie.services.common.utils.InstanceHolders;
  
-public class CommonAction  extends ActionSupport
+public class CommonAction  extends ActionSupport implements  ServletRequestAware
 {	
 	
     private static final long serialVersionUID = 1L;
     protected final  Logger logger = Logger.getLogger(this.getClass());
+  
     
-    public boolean statusDisplay;
-    
-	public boolean isStatusDisplay() {
-		return statusDisplay;
-	}
-
-	public void setStatusDisplay(boolean statusDisplay) {
-		this.statusDisplay = statusDisplay;
-	}	
-	
-	public String showConfigurationDetials()
-	{
-		return "SUCCESS";
-	}
 	//Directing to index page.
 	public String indexPage() throws SQLException{
 		
@@ -33,47 +30,22 @@ public class CommonAction  extends ActionSupport
 	
 	public String display() throws SQLException{ 
 		String sReturnStatus="ERROR";
-		Connection con=null;
-		try{
-			
-		}catch (Exception e) {
-			if(con!=null){
-				con.close();
-				con=null;
-			}
-		}
-		finally{
-			if(con!=null){
-				con.close();
-				con=null;
-			}
-		}
+		
     	return sReturnStatus;
     }
 	
-
-	/*
-	 * Configuration of database table.
-	 */
-	public String getDatabaseConfigurationPage() throws SQLException
+	public String showProviderUploadPage () throws SQLException, IOException
 	{
-		Connection con=null;
-		String sReturnStatus="ERROR";
-		try{
+		String sReturnStatus="SUCCESS";
 		
-		}catch (Exception e) {
-			if(con!=null){
-				con.close();
-				con=null;
-			}
-		}
-		finally{
-			if(con!=null){
-				con.close();
-				con=null;
-			}
-		}
 		return sReturnStatus;
 	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
        
 }
