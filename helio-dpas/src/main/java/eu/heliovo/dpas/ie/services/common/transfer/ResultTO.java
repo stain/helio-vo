eb32287c9100a9236d8229e1cddca667d8622d5f
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-24 10:50:38 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1182 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/transfer/ResultTO.java
@@ -0,0 +1,29 @@
+package eu.heliovo.dpas.ie.services.common.transfer;
+
+public class ResultTO {
+
+	private String helioInst;
+	private String inst;
+	private String providerType ;
+	
+	
+	public String getHelioInst() {
+		return helioInst;
+	}
+	public void setHelioInst(String helioInst) {
+		this.helioInst = helioInst;
+	}
+	public String getInst() {
+		return inst;
+	}
+	public void setInst(String inst) {
+		this.inst = inst;
+	}
+	public String getProviderType() {
+		return providerType;
+	}
+	public void setProviderType(String providerType) {
+		this.providerType = providerType;
+	}
+	
+}
