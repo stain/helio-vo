851143b6aa04d2df04c1a340e95308bba8e382e3
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-24 10:50:57 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1183 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/impl/ShortNameQueryDaoImpl.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/impl/ShortNameQueryDaoImpl.java
@@ -4,12 +4,10 @@ package eu.heliovo.dpas.ie.services.common.dao.impl;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Properties;
-
 import org.apache.log4j.Logger;
-
 import eu.heliovo.dpas.ie.services.common.dao.exception.DetailsNotFoundException;
 import eu.heliovo.dpas.ie.services.common.dao.interfaces.ShortNameQueryDao;
+import eu.heliovo.dpas.ie.services.common.transfer.ResultTO;
 import eu.heliovo.dpas.ie.services.common.utils.ConnectionManager;
 
 public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
@@ -61,8 +59,65 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 			} catch (Exception e) {
 				
 			}
-	}
-		
+		}
 	}
 	
-}
+	
+		@Override
+	public ResultTO[] getAccessTableBasedOnInst(String strIns) throws DetailsNotFoundException {
+			int count=0;
+			ResultTO[] resultTO=null;
+			try{
+				String query="select * from pat where helio_inst_id='"+strIns+"'";
+				System.out.println("loadProviderAccessTable() method "+strIns);
+				//Connecting to database.						
+				con = ConnectionManager.getConnection();
+				st = con.createStatement();
+				rs=st.executeQuery(query);
+				resultTO = new ResultTO[rs.getRow()];
+				while(rs.next()){
+					//Helio Inst
+					if(rs.getString(1)!=null)
+						resultTO[count].setHelioInst(rs.getString(1));
+					//Provider Type
+					if(rs.getString(1)!=null)
+						resultTO[count].setProviderType(rs.getString(1));
+					//Instrument
+					if(rs.getString(1)!=null)
+						resultTO[count].setInst(rs.getString(1));
+					count++;
+                }
+				con.commit();
+				//
+				return resultTO;
+			}catch(Exception e){
+				logger.fatal(" Exception occured in getUrlFromHsqlDB() : ",e);
+			}
+			
+			finally
+			{
+				try {
+					
+					if(rs!=null)
+					{
+						rs.close();
+						rs=null;
+					}
+					if(st!=null)
+					{
+						st.close();
+						st=null;
+					}
+					if(con!=null)
+					{
+						con.close();
+						con=null;
+					}
+				} catch (Exception e) {
+					
+				}
+		    }	
+			return null;
+		}
+	}
+
--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/interfaces/ShortNameQueryDao.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/interfaces/ShortNameQueryDao.java
@@ -5,9 +5,12 @@ import java.sql.Connection;
 import java.util.HashMap;
 
 import eu.heliovo.dpas.ie.services.common.dao.exception.DetailsNotFoundException;
+import eu.heliovo.dpas.ie.services.common.transfer.ResultTO;
 
 
 public interface ShortNameQueryDao {
 
 	public void loadProviderAccessTable(String fileName) throws DetailsNotFoundException;
+
+	public ResultTO[] getAccessTableBasedOnInst(String fileName) throws DetailsNotFoundException;
 }
