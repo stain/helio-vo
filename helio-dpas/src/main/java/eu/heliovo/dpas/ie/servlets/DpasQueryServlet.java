8b9c71856a6318ec59c706c9fb6d017116d4d391
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-27 13:01:05 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1200 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/DpasQueryServlet.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/DpasQueryServlet.java
@@ -14,6 +14,8 @@ import eu.heliovo.dpas.ie.common.VOTableCreator;
 import eu.heliovo.dpas.ie.controller.ServiceEngine;
 import eu.heliovo.dpas.ie.dataProviders.DPASDataProvider;
 import eu.heliovo.dpas.ie.services.cdaweb.dao.interfaces.CdaWebQueryDao;
+import eu.heliovo.dpas.ie.services.common.transfer.ResultTO;
+import eu.heliovo.dpas.ie.services.common.utils.HsqlDbUtils;
 import eu.heliovo.dpas.ie.services.uoc.dao.interfaces.UocQueryDao;
 import eu.heliovo.dpas.ie.services.vso.dao.interfaces.VsoQueryDao;
 import eu.heliovo.dpas.ie.services.vso.utils.VsoUtils;
@@ -43,8 +45,9 @@ public class DpasQueryServlet extends HttpServlet {
 		String startTime[]=null;
 		String stopTime[]=null;
 		ServiceEngine serviceEngine=null;
+		
 		try{
-			serviceEngine=new ServiceEngine();
+			 serviceEngine=new ServiceEngine();
 		     //Setting start time & end time parameter
 		     String sStartTime=request.getParameter("STARTTIME");
 		     String sEndTime=request.getParameter("ENDTIME");			
@@ -69,35 +72,45 @@ public class DpasQueryServlet extends HttpServlet {
 				 VOTableCreator.writeHeaderOfTables(commonTO);
 				 //For loop
 		    	 for(int count=0;count<instruments.length;count++){
-			    	 commonTO.setInstrument(instruments[count].split("__")[1]);
-			    	 commonTO.setDateFrom(startTime[count]);
-			    	 commonTO.setDateTo(stopTime[count]);
-			    	 commonTO.setWhichProvider(instruments[count].split("__")[0]);
-				     //Calling DAO factory to connect PROVIDERS
-				     if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof VsoQueryDao ){
-				    	 commonTO.setVotableDescription("VSO query response");
-				    	 commonTO.setUrl(VsoUtils.getUrl(request));
-				    	 VsoQueryDao vsoQueryDao= (VsoQueryDao) DAOFactory.getDAOFactory(commonTO.getWhichProvider());
-			         	 vsoQueryDao.query(commonTO);
-				     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof UocQueryDao ){
-				    	 UocQueryDao uocQueryDao=(UocQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
-				    	 uocQueryDao.query(commonTO);
-				     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof CdaWebQueryDao ){
-				    	 commonTO.setVotableDescription("CDAWEB query response");
-				    	 CdaWebQueryDao cdaWebQueryDao=(CdaWebQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
-				    	 cdaWebQueryDao.query(commonTO);
-				     }else{
-				    	 /*String []instr = new String[1];
-				    	 instr[0]="RHESSI__HESSI_GMR";
-				    	 //
-				    	 String []starttime = new String[1];
-				    	 starttime[0]=commonTO.getDateFrom();
-				    	 //
-				    	 String []stoptime = new String[1];
-				    	 stoptime[0]=commonTO.getDateTo();
-				    	 serviceEngine.executeQuery(pw,instr,starttime,stoptime,false, null, null,true);
-				    	 */
-				     }
+		    		 //getting details from Provider access table
+		    		 ResultTO[] resultTo=HsqlDbUtils.getInstance().getAccessTableBasedOnInst(instruments[count]);
+		    		 if(resultTo!=null && resultTo.length>0){
+				    	 commonTO.setInstrument(resultTo[0].getInst());
+				    	 commonTO.setDateFrom(startTime[count]);
+				    	 commonTO.setDateTo(stopTime[count]);
+				    	 commonTO.setWhichProvider(resultTo[0].getProviderType());
+					     //Calling DAO factory to connect PROVIDERS
+					     if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof VsoQueryDao ){
+					    	 commonTO.setVotableDescription("VSO query response");
+					    	 commonTO.setUrl(VsoUtils.getUrl(request));
+					    	 VsoQueryDao vsoQueryDao= (VsoQueryDao) DAOFactory.getDAOFactory(commonTO.getWhichProvider());
+				         	 vsoQueryDao.query(commonTO);
+					     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof UocQueryDao ){
+					    	 commonTO.setVotableDescription("UOC query response");
+					    	 UocQueryDao uocQueryDao=(UocQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
+					    	 uocQueryDao.query(commonTO);
+					     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof CdaWebQueryDao ){
+					    	 commonTO.setVotableDescription("CDAWEB query response");
+					    	 CdaWebQueryDao cdaWebQueryDao=(CdaWebQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
+					    	 cdaWebQueryDao.query(commonTO);
+					     }else{
+					    	 String []instr = new String[1];
+					    	 instr[0]="RHESSI__HESSI_GMR";
+					    	 //
+					    	 String []starttime = new String[1];
+					    	 starttime[0]=commonTO.getDateFrom();
+					    	 //
+					    	 String []stoptime = new String[1];
+					    	 stoptime[0]=commonTO.getDateTo();
+					    	 serviceEngine.executeQuery(pw,instr,starttime,stoptime,false, null, null,true);
+					     }
+		    		 }else{
+		    			 commonTO.setBufferOutput(new BufferedWriter(pw));
+				    	 commonTO.setVotableDescription("Error votable response, no data");
+				    	 commonTO.setQuerystatus("ERROR");
+				    	 commonTO.setQuerydescription("No data avialable for Instrument: "+instruments[count]);
+						 VOTableCreator.writeErrorTables(commonTO);
+		    		 }
 		    	 }
 		    	//VOTable footer.
 				VOTableCreator.writeFooterOfTables(commonTO);
