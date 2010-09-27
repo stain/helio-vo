310dc1c90153b0e758a56d1fce206ad8281c491c
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-27 12:59:59 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1195 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/impl/ShortNameQueryDaoImpl.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/impl/ShortNameQueryDaoImpl.java
@@ -10,6 +10,7 @@ import eu.heliovo.dpas.ie.services.common.dao.interfaces.ShortNameQueryDao;
 import eu.heliovo.dpas.ie.services.common.transfer.ResultTO;
 import eu.heliovo.dpas.ie.services.common.utils.ConnectionManager;
 
+
 public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 		
 	public ShortNameQueryDaoImpl(){ 
@@ -30,7 +31,7 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 			//Connecting to database.						
 			con = ConnectionManager.getConnection();
 			st = con.createStatement();
-			st.executeQuery(query);
+			st.execute(query);
 			con.commit();
 			
 		}catch(Exception e){
@@ -68,30 +69,45 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 			int count=0;
 			ResultTO[] resultTO=null;
 			try{
-				String query="select * from pat where helio_inst_id='"+strIns+"'";
-				System.out.println("loadProviderAccessTable() method "+strIns);
+				String query="select * from pat where helio_obs_inst='"+strIns+"'";
+				System.out.println("  :  getAccessTableBasedOnInst() method :  "+strIns);
 				//Connecting to database.						
 				con = ConnectionManager.getConnection();
 				st = con.createStatement();
 				rs=st.executeQuery(query);
-				resultTO = new ResultTO[rs.getRow()];
+				//rs.last();
+				resultTO = new ResultTO[1];
+				//rs.first();
 				while(rs.next()){
+					resultTO[count]=new ResultTO();
 					//Helio Inst
 					if(rs.getString(1)!=null)
 						resultTO[count].setHelioInst(rs.getString(1));
 					//Provider Type
-					if(rs.getString(1)!=null)
-						resultTO[count].setProviderType(rs.getString(1));
+					if(rs.getString(2)!=null)
+						resultTO[count].setProviderType(rs.getString(2));
 					//Instrument
-					if(rs.getString(1)!=null)
-						resultTO[count].setInst(rs.getString(1));
+					if(rs.getString(3)!=null)
+						resultTO[count].setInst(rs.getString(3));
+					//Obsevatory Id
+					if(rs.getString(4)!=null)
+						resultTO[count].setObsId(rs.getString(4));
+					//Provider Ranking
+					if(rs.getString(5)!=null)
+						resultTO[count].setPvdrRanking(rs.getString(5));
+					//Provider quality
+					if(rs.getString(6)!=null)
+						resultTO[count].setPvdrQuality(rs.getString(6));
+					//Vso Int id
+					if(rs.getString(7)!=null)
+						resultTO[count].setPvdrVsoKey(rs.getString(7));
 					count++;
                 }
-				con.commit();
-				//
+	
 				return resultTO;
 			}catch(Exception e){
-				logger.fatal(" Exception occured in getUrlFromHsqlDB() : ",e);
+				e.printStackTrace();
+				logger.fatal(" Exception occured in getAccessTableBasedOnInst() : ",e);
 			}
 			
 			finally
