7c648300e1aaeaf73de0f390fe14e47c0a73c0df
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-09-30 11:07:15 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1226 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/StartupServlet.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/StartupServlet.java
@@ -4,6 +4,8 @@ package eu.heliovo.dpas.ie.servlets;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+
+import eu.heliovo.dpas.ie.services.common.utils.HsqlDbUtils;
 import eu.heliovo.dpas.ie.services.common.utils.InstanceHolders;
 
 public class StartupServlet extends HttpServlet {
@@ -14,12 +16,19 @@ public class StartupServlet extends HttpServlet {
 		super.init(config);
 		try{
 			 // getting the path.
+			System.out.println("---> getting HSQL database path -------->");
 			 String sProfileFilePath=getServletContext().getRealPath("/");
 			 if(sProfileFilePath!=null && !sProfileFilePath.equals("")){
 				 	sProfileFilePath=sProfileFilePath+ "WEB-INF";
 					InstanceHolders.getInstance().setProperty("hsqldb.database.path",sProfileFilePath);
 					System.out.println(" : HSQLDB database location : "+sProfileFilePath);
 			 }
+			System.out.println("---> done database path configuration-------->");
+			System.out.println("---> Setting PAT table -------->");
+			//Setting .txt for 'pat' table.
+			HsqlDbUtils.getInstance().loadProviderAccessTable("pat.txt");	
+			InstanceHolders.getInstance().setProperty("patFileName","pat.txt");
+			System.out.println("---> Done configuration of PAT table -------->");
 		}
 		catch(Exception ex)
 		{
