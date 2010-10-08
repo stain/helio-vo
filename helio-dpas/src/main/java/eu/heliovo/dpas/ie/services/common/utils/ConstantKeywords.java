eb27ac1ba561c6e9394a5c729d82222535678c24
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-08 09:33:42 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1231 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/utils/ConstantKeywords.java
@@ -0,0 +1,19 @@
+package eu.heliovo.dpas.ie.services.common.utils;
+
+public enum ConstantKeywords {
+	
+	VSODATEFORMAT("yyyyMMddHHmmss"),
+	ORGINALDATEFORMAT("yyyy-MM-dd HH:mm:ss");
+	
+	
+    private String dateformat;
+ 
+    private ConstantKeywords(String dateformat) {
+    	this.dateformat = dateformat;
+    }
+
+    public String getDateFormat() {
+    	return dateformat;
+    }
+
+};
