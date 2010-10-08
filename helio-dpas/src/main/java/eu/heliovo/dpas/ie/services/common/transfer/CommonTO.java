f7c051ac761befb066665da7d57831c5e84f6fa1
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-08 09:41:42 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1239 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/transfer/CommonTO.java
@@ -0,0 +1,215 @@
+package eu.heliovo.dpas.ie.services.common.transfer;
+
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+
+import uk.ac.starlink.table.StarTable;
+
+public class CommonTO {
+
+	private String[] instruments;
+	private String[] startTimes;
+	private String[] stopTimes ;
+	private boolean partialSorting;
+	private String[] dataTypes;
+	private int[] dataLevels;
+	private Writer printWriter;
+	private boolean votable;
+	private String instrument;
+	private String dateFrom;
+	private String dateTo;
+	private  String url;
+	private String status;
+	private String querystatus;
+	private int maxResults;
+	private String querydescription;
+	private StarTable starTable;
+	private BufferedWriter bufferOutput;
+	private String votableDescription;
+	private String whichProvider; 
+	private StarTable[] starTableArray;
+	private HttpServletRequest request;
+	private String missionName;
+	private String From;
+	private String exceptionStatus;
+	
+	public StarTable[] getStarTableArray() {
+		return starTableArray;
+	}
+
+	public void setStarTableArray(StarTable[] starTableArray) {
+		this.starTableArray = starTableArray;
+	}
+
+	public StarTable getStarTable() {
+		return starTable;
+	}
+
+	public void setStarTable(StarTable starTable) {
+		this.starTable = starTable;
+	}
+
+	public BufferedWriter getBufferOutput() {
+		return bufferOutput;
+	}
+
+	public void setBufferOutput(BufferedWriter bufferOutput) {
+		this.bufferOutput = bufferOutput;
+	}
+	
+	public String getWhichProvider() {
+		return whichProvider;
+	}
+
+	public void setWhichProvider(String whichProvider) {
+		this.whichProvider = whichProvider;
+	}
+
+	public String getInstrument() {
+		return instrument;
+	}
+	public void setInstrument(String instrument) {
+		this.instrument = instrument;
+	}
+	public String getDateFrom() {
+		return dateFrom;
+	}
+	public void setDateFrom(String dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+	public String getDateTo() {
+		return dateTo;
+	}
+	public void setDateTo(String dateTo) {
+		this.dateTo = dateTo;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public int getMaxResults() {
+		return maxResults;
+	}
+	public void setMaxResults(int maxResults) {
+		this.maxResults = maxResults;
+	}
+	
+	
+	public String[] getInstruments() {
+		return instruments;
+	}
+	public void setInstruments(String[] instruments) {
+		this.instruments = instruments;
+	}
+	public String[] getStartTimes() {
+		return startTimes;
+	}
+	public void setStartTimes(String[] startTimes) {
+		this.startTimes = startTimes;
+	}
+	public String[] getStopTimes() {
+		return stopTimes;
+	}
+	public void setStopTimes(String[] stopTimes) {
+		this.stopTimes = stopTimes;
+	}
+	public boolean isPartialSorting() {
+		return partialSorting;
+	}
+	public void setPartialSorting(boolean partialSorting) {
+		this.partialSorting = partialSorting;
+	}
+	public String[] getDataTypes() {
+		return dataTypes;
+	}
+	public void setDataTypes(String[] dataTypes) {
+		this.dataTypes = dataTypes;
+	}
+	public int[] getDataLevels() {
+		return dataLevels;
+	}
+	public void setDataLevels(int[] dataLevels) {
+		this.dataLevels = dataLevels;
+	}
+	public Writer getPrintWriter() {
+		return printWriter;
+	}
+	public void setPrintWriter(Writer printWriter) {
+		this.printWriter = printWriter;
+	}
+	public boolean isVotable() {
+		return votable;
+	}
+	public void setVotable(boolean votable) {
+		this.votable = votable;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getQuerystatus() {
+		return querystatus;
+	}
+
+	public void setQuerystatus(String querystatus) {
+		this.querystatus = querystatus;
+	}
+
+	public String getQuerydescription() {
+		return querydescription;
+	}
+
+	public void setQuerydescription(String querydescription) {
+		this.querydescription = querydescription;
+	}
+
+	public String getVotableDescription() {
+		return votableDescription;
+	}
+
+	public void setVotableDescription(String votableDescription) {
+		this.votableDescription = votableDescription;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public String getMissionName() {
+		return missionName;
+	}
+
+	public void setMissionName(String missionName) {
+		this.missionName = missionName;
+	}
+
+	public String getFrom() {
+		return From;
+	}
+
+	public void setFrom(String from) {
+		From = from;
+	}
+
+	public String getExceptionStatus() {
+		return exceptionStatus;
+	}
+
+	public void setExceptionStatus(String exceptionStatus) {
+		this.exceptionStatus = exceptionStatus;
+	}	
+	
+}
