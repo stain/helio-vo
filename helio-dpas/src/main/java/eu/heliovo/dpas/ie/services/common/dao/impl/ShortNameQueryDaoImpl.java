7a01c8770b834b308b46abf1eea55ce7dc6b5c92
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-15 10:27:10 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1270 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/impl/ShortNameQueryDaoImpl.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/dao/impl/ShortNameQueryDaoImpl.java
@@ -26,6 +26,7 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 	public void loadProviderAccessTable(String fileName) throws DetailsNotFoundException {
 		String sUrl=null;
 		try{
+			System.out.println("  :  -----> Setting uploaded file for provider access table ----->");
 			String query="SET TABLE pat SOURCE "+"\""+fileName+"\"";
 			System.out.println("loadProviderAccessTable() method "+query);
 			//Connecting to database.						
@@ -33,7 +34,7 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 			st = con.createStatement();
 			st.execute(query);
 			con.commit();
-			
+			System.out.println("  :  -----> Done ----->");
 		}catch(Exception e){
 			logger.fatal(" Exception occured in loadProviderAccessTable() : ",e);
 			throw new DetailsNotFoundException("Exception: ",e);
@@ -70,8 +71,8 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 			int count=0;
 			ResultTO[] resultTO=null;
 			try{
-				String query="select * from pat where helio_obs_inst='"+strIns+"'";
-				System.out.println("  :  getAccessTableBasedOnInst() method :  "+strIns);
+				String query="select * from pat where helio_obs_inst='"+strIns+"' order by pvdr_ranking limit 1";
+				System.out.println("  :  -----> getAccessTableBasedOnInst() method -----> : Reslut For "+strIns);
 				//Connecting to database.						
 				con = ConnectionManager.getConnection();
 				st = con.createStatement();
@@ -84,27 +85,36 @@ public class ShortNameQueryDaoImpl implements ShortNameQueryDao {
 					//Helio Inst
 					if(rs.getString(1)!=null)
 						resultTO[count].setHelioInst(rs.getString(1));
-					//Provider Type
+					//Provider Name
 					if(rs.getString(2)!=null)
-						resultTO[count].setProviderType(rs.getString(2));
-					//Instrument
+						resultTO[count].setProviderName(rs.getString(2));
+					//Provider Type
 					if(rs.getString(3)!=null)
-						resultTO[count].setInst(rs.getString(3));
-					//Obsevatory Id
+						resultTO[count].setProviderType(rs.getString(3));
+					//Instrument
 					if(rs.getString(4)!=null)
-						resultTO[count].setObsId(rs.getString(4));
-					//Provider Ranking
+						resultTO[count].setInst(rs.getString(4));
+					//Obsevatory Id
 					if(rs.getString(5)!=null)
-						resultTO[count].setPvdrRanking(rs.getString(5));
-					//Provider quality
+						resultTO[count].setObsId(rs.getString(5));
+					//Provider Source
 					if(rs.getString(6)!=null)
-						resultTO[count].setPvdrQuality(rs.getString(6));
-					//Vso Int id
+						resultTO[count].setProviderSource(rs.getString(6));
+					//Provider Ack
 					if(rs.getString(7)!=null)
-						resultTO[count].setPvdrVsoKey(rs.getString(7));
+						resultTO[count].setProviderAck(rs.getString(7));
+					//Provider Ranking
+					if(rs.getString(8)!=null)
+						resultTO[count].setPvdrRanking(rs.getString(8));
+					//Provider quality
+					if(rs.getString(9)!=null)
+						resultTO[count].setPvdrQuality(rs.getString(9));
+					//Vso Int id
+					if(rs.getString(10)!=null)
+						resultTO[count].setPvdrVsoKey(rs.getString(10));
 					count++;
                 }
-	
+				System.out.println("  :  -----> Success -----> Reslut For "+strIns);
 				return resultTO;
 			}catch(Exception e){
 				e.printStackTrace();
