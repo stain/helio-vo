3d012dd29216dd7dbdbac7a1fa0aad7180ffa5dc
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-15 10:17:27 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1269 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/directory/utils/DirInsAnlyFactory.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/directory/utils/DirInsAnlyFactory.java
@@ -1,10 +1,12 @@
 package eu.heliovo.dpas.ie.services.directory.utils;
 
 import eu.heliovo.dpas.ie.services.common.dao.interfaces.DPASDataProvider;
+import eu.heliovo.dpas.ie.services.directory.provider.NOBEProvider;
 import eu.heliovo.dpas.ie.services.directory.provider.Phoenix2Provider;
 import eu.heliovo.dpas.ie.services.directory.provider.RhessiProvider;
 import eu.heliovo.dpas.ie.services.directory.provider.SOT_FGProvider;
 import eu.heliovo.dpas.ie.services.directory.provider.SOT_SPProvider;
+import eu.heliovo.dpas.ie.services.directory.provider.XRTProvider;
 
 public abstract class DirInsAnlyFactory {
 	  
@@ -22,6 +24,10 @@ public abstract class DirInsAnlyFactory {
 	    	  return new SOT_SPProvider();
 	      case HINODE__SOT_FG		:
 	    	  return new SOT_FGProvider();
+	      case HINODE__XRT		:
+	    	  return new XRTProvider();
+	      case NOBE__NORH		:
+	    	  return new NOBEProvider();
 	      default        : 
 	          return null;
 	    }
@@ -29,7 +35,7 @@ public abstract class DirInsAnlyFactory {
 	   	  
 
 	enum DirType{
-		PHOENIX__2,RHESSI__HESSI_GMR,RHESSI__HESSI_HXR,HINODE__SOT_SP,HINODE__SOT_FG
+		PHOENIX__2,RHESSI__HESSI_GMR,RHESSI__HESSI_HXR,HINODE__SOT_SP,HINODE__SOT_FG,HINODE__XRT,NOBE__NORH
 	};
 
 
