5d25ee52ccbab83b71f65457dfc2c5bca21e26ec
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-15 10:27:23 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1271 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/transfer/ResultTO.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/transfer/ResultTO.java
@@ -3,9 +3,12 @@ package eu.heliovo.dpas.ie.services.common.transfer;
 public class ResultTO {
 
 	private String helioInst;
+	private String providerType;
+	private String providerName;
 	private String inst;
-	private String providerType ;
 	private String obsId;
+	private String providerSource;
+	private String providerAck;
 	private String pvdrRanking;
 	private String pvdrQuality;
 	private String pvdrVsoKey;
@@ -52,5 +55,25 @@ public class ResultTO {
 	public void setPvdrVsoKey(String pvdrVsoKey) {
 		this.pvdrVsoKey = pvdrVsoKey;
 	}
+	public String getProviderName() {
+		return providerName;
+	}
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+	public String getProviderSource() {
+		return providerSource;
+	}
+	public void setProviderSource(String providerSource) {
+		this.providerSource = providerSource;
+	}
+	public String getProviderAck() {
+		return providerAck;
+	}
+	public void setProviderAck(String providerAck) {
+		this.providerAck = providerAck;
+	}
+	
+	
 	
 }
