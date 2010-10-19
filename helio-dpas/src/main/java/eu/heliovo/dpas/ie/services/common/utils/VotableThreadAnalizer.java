bd613b15d991a6536853f9718084a00aab324786
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-19 10:16:24 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1286 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/utils/VotableThreadAnalizer.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/utils/VotableThreadAnalizer.java
@@ -6,6 +6,7 @@ import eu.heliovo.dpas.ie.services.cdaweb.dao.interfaces.CdaWebQueryDao;
 import eu.heliovo.dpas.ie.services.common.transfer.CommonTO;
 import eu.heliovo.dpas.ie.services.common.transfer.ResultTO;
 import eu.heliovo.dpas.ie.services.common.utils.HsqlDbUtils;
+import eu.heliovo.dpas.ie.services.directory.dao.interfaces.DirQueryDao;
 import eu.heliovo.dpas.ie.services.uoc.dao.interfaces.UocQueryDao;
 import eu.heliovo.dpas.ie.services.vso.dao.interfaces.VsoQueryDao;
 import eu.heliovo.dpas.ie.services.vso.utils.VsoUtils;
@@ -30,14 +31,14 @@ public class VotableThreadAnalizer extends Thread
 			    //VOTable header
 				 VOTableCreator.writeHeaderOfTables(commonTO);
 				 //For loop
-				 for(int count=0;count<instruments.length;count++){
+		    	 for(int count=0;count<instruments.length;count++){
 		    		 //getting details from Provider access table
 		    		 ResultTO[] resultTo=HsqlDbUtils.getInstance().getAccessTableBasedOnInst(instruments[count]);
 		    		 if(resultTo!=null && resultTo.length>0 && resultTo[0]!=null){
 				    	 commonTO.setInstrument(resultTo[0].getInst());
 				    	 commonTO.setDateFrom(startTime[count]);
 				    	 commonTO.setDateTo(stopTime[count]);
-				    	 commonTO.setWhichProvider(resultTo[0].getProviderType());
+				    	 commonTO.setWhichProvider(resultTo[0].getProviderName());
 					     //Calling DAO factory to connect PROVIDERS
 					     if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof VsoQueryDao ){
 					    	 commonTO.setVotableDescription("VSO query response");
@@ -46,6 +47,7 @@ public class VotableThreadAnalizer extends Thread
 				         	 vsoQueryDao.query(commonTO);
 					     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof UocQueryDao ){
 					    	 commonTO.setVotableDescription("UOC query response");
+					    	 commonTO.setInstrument(resultTo[0].getObsId());
 					    	 UocQueryDao uocQueryDao=(UocQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
 					    	 uocQueryDao.query(commonTO);
 					     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof CdaWebQueryDao ){
@@ -53,19 +55,14 @@ public class VotableThreadAnalizer extends Thread
 					    	 commonTO.setVotableDescription("CDAWEB query response");
 					    	 CdaWebQueryDao cdaWebQueryDao=(CdaWebQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
 					    	 cdaWebQueryDao.query(commonTO);
-					     }else{
-					    	 String []instr = new String[1];
-					    	 instr[0]="RHESSI__HESSI_GMR";
-					    	 //
-					    	 String []starttime = new String[1];
-					    	 starttime[0]=commonTO.getDateFrom();
-					    	 //
-					    	 String []stoptime = new String[1];
-					    	 stoptime[0]=commonTO.getDateTo();
-					    	 //serviceEngine.executeQuery(pw,instr,starttime,stoptime,false, null, null,true);
+					     }else if(DAOFactory.getDAOFactory(commonTO.getWhichProvider()) instanceof DirQueryDao ){
+					    	 commonTO.setVotableDescription("Archive query response");
+					    	 DirQueryDao dirQueryDao=(DirQueryDao)DAOFactory.getDAOFactory(commonTO.getWhichProvider());
+					    	 dirQueryDao.query(commonTO);
 					     }
 		    		 }else{
-				    	 commonTO.setVotableDescription("Error votable response, no data for");
+		    			 //commonTO.setBufferOutput(new BufferedWriter(pw));
+				    	 commonTO.setVotableDescription("Error votable response, no data");
 				    	 commonTO.setQuerystatus("ERROR");
 				    	 commonTO.setQuerydescription("No data avialable for Instrument: "+instruments[count]);
 						 VOTableCreator.writeErrorTables(commonTO);
