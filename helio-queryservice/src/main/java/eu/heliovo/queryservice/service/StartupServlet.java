49bc3e2c699fb3f5621f3ccbdd3501f73957428f
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-10-20 10:24:50 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1308 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/service/StartupServlet.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/service/StartupServlet.java
@@ -14,14 +14,20 @@ public class StartupServlet extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 
 		try{
-			 ClassLoader loader = this.getClass().getClassLoader();
+			 /*ClassLoader loader = this.getClass().getClassLoader();
 			 // check id test.txt available.
 			 String sProfileFilePath=loader.getResource("test.txt").getFile();
 			 if(sProfileFilePath!=null && !sProfileFilePath.equals("")){
 					InstanceHolders.getInstance().setProperty("hsqldb.database.path",sProfileFilePath.replaceAll("/test.txt", ""));
 					System.out.println(" : HSQLDB database location : "+sProfileFilePath.replaceAll("/test.txt", ""));
 			 }
-			
+			*/
+			String sProfileFilePath=getServletContext().getRealPath("/");
+			if(sProfileFilePath!=null && !sProfileFilePath.equals("")){
+				 	sProfileFilePath=sProfileFilePath+ "WEB-INF";
+					InstanceHolders.getInstance().setProperty("hsqldb.database.path",sProfileFilePath);
+					System.out.println(" : HSQLDB database location : "+sProfileFilePath);
+			}
 		}
 		catch(Exception ex)
 		{
