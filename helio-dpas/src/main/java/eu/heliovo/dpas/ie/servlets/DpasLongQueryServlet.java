d70ff84d1231afb4f7f4d0cdb831b6c012a0c5ca
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-11-18 15:14:44 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1379 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/DpasLongQueryServlet.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/servlets/DpasLongQueryServlet.java
@@ -1,29 +1,37 @@
 package eu.heliovo.dpas.ie.servlets;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.UUID;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import eu.heliovo.dpas.ie.controller.ServiceEngine;
-import eu.heliovo.dpas.ie.services.CommonDaoFactory;
-import eu.heliovo.dpas.ie.services.common.dao.interfaces.ShortNameQueryDao;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
 import eu.heliovo.dpas.ie.services.common.transfer.CommonTO;
+import eu.heliovo.dpas.ie.services.common.transfer.FileResultTO;
 import eu.heliovo.dpas.ie.services.common.utils.CommonUtils;
+import eu.heliovo.dpas.ie.services.common.utils.FileUtils;
+import eu.heliovo.dpas.ie.services.common.utils.InstanceHolders;
+import eu.heliovo.dpas.ie.services.common.utils.RunService;
 import eu.heliovo.dpas.ie.services.common.utils.VOTableCreator;
 
 /**
  * Servlet implementation class DpasQueryServlet
  */
-public class DpasQueryServlet extends HttpServlet {
+public class DpasLongQueryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DpasQueryServlet() {
+    public DpasLongQueryServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -39,6 +47,7 @@ public class DpasQueryServlet extends HttpServlet {
 		String startTime[]=null;
 		String stopTime[]=null;
 		commonTO.setRequest(request);
+		FileResultTO fileTO=new FileResultTO();
 		try{
 		     //Setting start time & end time parameter
 		     String sStartTime=request.getParameter("STARTTIME");
@@ -54,9 +63,8 @@ public class DpasQueryServlet extends HttpServlet {
 		     //Instrument
 		     if(sInstrument!=null && !sInstrument.trim().equals(""))
 		    	 instruments=sInstrument.split(",");
+		     String saveTo=request.getParameter("SAVETO");
 		     // Setting Print Writer.
-		     commonTO.setPrintWriter(pw);
-		     commonTO.setBufferOutput(new BufferedWriter(pw) );
 		     commonTO.setContextUrl(CommonUtils.getUrl(request));
 		     commonTO.setAllDateFrom(sStartTime);
 		     commonTO.setAllDateTo(sEndTime);
@@ -64,9 +72,55 @@ public class DpasQueryServlet extends HttpServlet {
 		     commonTO.setInstruments(instruments);
 		     commonTO.setStartTimes(startTime);
 		     commonTO.setStopTimes(stopTime);
+		     // Save To file.
+			if(saveTo==null || saveTo==""){
+				    saveTo= InstanceHolders.getInstance().getProperty("hsqldb.database.path")+"/files";
+				    File f = new File(saveTo);
+				    //Checking if directry present; if not create one.
+				    if(!f.exists())
+				    	f.mkdir();
+				    //passing save to value to common TO.	
+				   System.out.println(" : save to file location :  "+saveTo);
+			}
+			//Save To
+			commonTO.setSaveto(saveTo);
+			 //Creating UUID and generating unique ID.
+		    UUID uuid = UUID.randomUUID();
+		    String randomUUIDString = uuid.toString();
+		    //Checking for file or ftp.
+		    File file=null;
+		    BufferedWriter bw =null;
+		    String saveFilePath=null;
+		    //
+		    if(saveTo!=null && saveTo.trim().contains("ftp")){
+		    	FileUtils.saveFileToFtp(saveTo,"votable_"+randomUUIDString+".xml");	    			    	
+		    }else{
+		    	 saveFilePath=saveTo+"/votable_"+randomUUIDString+".xml";
+		    	 file = new File(saveFilePath);
+			     bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+		    }
 		    
-		     ShortNameQueryDao shortNameDao= CommonDaoFactory.getInstance().getShortNameQueryDao();
-		     shortNameDao.generateVOTable(commonTO);
+		   //Setting print writer.
+		    commonTO.setPrintWriter(bw);
+		    //Buffered output
+		    commonTO.setBufferOutput(bw);
+		    //Status set to inform long running query
+		    commonTO.setLongRunningQueryStatus("LongRunning");
+		    //Running the service in back round
+		    RunService oRunReport= new RunService(commonTO,randomUUIDString);
+			Thread th = new Thread(oRunReport);
+			th.start();
+			
+			//Creating a xml file 
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder docBuilder = builderFactory.newDocumentBuilder();
+	        //creating a new instance of a DOM to build a DOM tree.
+	        Document doc = docBuilder.newDocument();
+	        //setting file TO.
+	        fileTO.setRandomUUIDString(randomUUIDString);
+	        String xmlString=CommonUtils.createXmlForWebService(fileTO);
+			System.out.println(" : XML String : "+xmlString);
+			pw.write(xmlString);
 		}catch(Exception e){
 			e.printStackTrace();
 			System.out.println(" : Exception occured while creating the file :  "+e.getMessage());
