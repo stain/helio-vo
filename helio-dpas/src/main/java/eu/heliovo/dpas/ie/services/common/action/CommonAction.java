de252be89180083b250364448872c31b0bdc645b
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-11-24 15:30:18 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1412 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/action/CommonAction.java
+++ helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/common/action/CommonAction.java
@@ -1,17 +1,17 @@
 package eu.heliovo.dpas.ie.services.common.action;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import java.util.Iterator;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
-
 import com.opensymphony.xwork2.ActionSupport;
-
 import eu.heliovo.dpas.ie.services.common.utils.CommonUtils;
 import eu.heliovo.dpas.ie.services.common.utils.InstanceHolders;
  
@@ -39,6 +39,52 @@ public class CommonAction  extends ActionSupport implements  ServletRequestAware
 		
     	return sReturnStatus;
     }
+	
+	
+	private String showLogs;
+	
+	public String getShowLogs() {
+		return showLogs;
+	}
+
+	public void setShowLogs(String showLogs) {
+		this.showLogs = showLogs;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String showLogFile()
+	{
+		String status="SUCCESS";
+		Map<String, String> hmbDt=CommonUtils.getLogLocations();
+		Iterator it = hmbDt.entrySet().iterator();
+		String logFileName="";
+	    while (it.hasNext()) {
+	        Map.Entry pairs = (Map.Entry)it.next();
+	        System.out.println("Log file name  = " + pairs.getValue());
+	        if(pairs!=null){
+	        	logFileName=pairs.getValue().toString();
+	    	} 
+	        break;
+	    }
+	    //
+	    try {
+			FileInputStream fileInputStream=new FileInputStream(new File(logFileName));
+			StringBuffer sb=CommonUtils.readInputStreamAsString(fileInputStream);
+			System.out.println(" -----------> String Buffer ---------->"+sb);
+			setShowLogs(sb.toString());
+			//Setting flag to true, database details are correct.
+			setStatusDisplay(true);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return status;
+	}
+	
 	
 	private boolean statusDisplay;
 	private String uploadedFileName;
