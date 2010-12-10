4ca219ce2141d15ec05e958be6681a6e692cb5f1
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-12-10 15:47:21 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1439 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-queryservice/src/main/java/eu/heliovo/queryservice/service/StartupServlet.java
+++ helio-queryservice/src/main/java/eu/heliovo/queryservice/service/StartupServlet.java
@@ -3,13 +3,19 @@ package eu.heliovo.queryservice.service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.PropertyConfigurator;
 
+import eu.heliovo.queryservice.common.dao.exception.DetailsNotFoundException;
+import eu.heliovo.queryservice.common.util.CommonUtils;
+import eu.heliovo.queryservice.common.util.DeleteStatusTimer;
 import eu.heliovo.queryservice.common.util.HsqlDbUtils;
 import eu.heliovo.queryservice.common.util.InstanceHolders;
 
@@ -26,7 +32,34 @@ public class StartupServlet extends HttpServlet {
 					InstanceHolders.getInstance().setProperty("hsqldb.database.path",sProfileFilePath);
 					//switchAppenders("jdbc:hsqldb:file:");
 					System.out.println(" : HSQLDB database location : "+sProfileFilePath);
+					switchAppenders();
 			}
+			//
+			InstanceHolders.getInstance().setProperty("hsql.status.delete.date",CommonUtils.date2String(new Date()));
+			//
+			TimerTask task = new DeleteStatusTimer() {
+				@Override
+				 protected void execute(){
+			      	try{			      		
+			      		int noOfMonth=CommonUtils.getNoOfMonths(InstanceHolders.getInstance().getProperty("hsql.status.delete.date"));
+			      		System.out.println(" ------ No of months ------"+noOfMonth );
+			      		if(noOfMonth>=6){
+			      			deleteLongRunningQueryStatus();
+			      			InstanceHolders.getInstance().setProperty("hsql.status.delete.date",CommonUtils.date2String(new Date()));
+			      		}
+			      		//System.out.println("-------- End ------------");
+				    }catch (Exception e){
+				    	System.out.println(" :  Exception occured in while deleting Long running query saved status and url "+ e);
+			      	}
+			    }
+			};
+
+			Timer timer = new Timer();
+			// check every 2 day 
+			timer.schedule(task , new Date(), 86400000*2); // refresh after 86400000 miliseconds  = 2 day
+			
+			
+			
 		}
 		catch(Exception ex)
 		{
@@ -36,10 +69,8 @@ public class StartupServlet extends HttpServlet {
 	}
 	
 	
-	private void switchAppenders(String url) {
+	private void switchAppenders() {
 	      Properties props = new Properties();
-	      url=url+InstanceHolders.getInstance().getProperty("hsqldb.database.path") +"/HelioDB/testdb;hsqldb.default_table_type=cached;shutdown=true";
-	      System.out.println(" ------------> : URL of LOGS Table : ---------->"+url);
 	      try {
 	           InputStream configStream = getClass().getResourceAsStream("/log4j.properties");
 	           props.load(configStream);
@@ -47,12 +78,18 @@ public class StartupServlet extends HttpServlet {
 	      } catch(IOException e) {
 	          System.out.println("Error: Cannot laod configuration file ");
 	      }
-	      props.setProperty("log4j.appender.DB.URL",url);
+	      props.setProperty("log4j.appender.LOGFILE.File",InstanceHolders.getInstance().getProperty("hsqldb.database.path")+"/query_service.log");
 	      LogManager.resetConfiguration();
 	      PropertyConfigurator.configure(props);
 	     }
 
-	
+	private void deleteLongRunningQueryStatus() throws DetailsNotFoundException{
+		System.out.println("----------- Deleting Long Running Query --------------");
+		//Deleting Status from JOB_STATUS
+		HsqlDbUtils.getInstance().deleteStatusFromHsqlDB();
+		//Deleting Status from JOB_URL
+		HsqlDbUtils.getInstance().deleteUrlFromHsqlDB();
+	}
 
 }
 
