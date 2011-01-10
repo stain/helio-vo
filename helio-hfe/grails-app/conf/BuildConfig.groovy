85170fc5ee1c1edfc978545b73b3149d900bd418
ennosigaeus <ennosigaeus@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-01-10 16:15:11 +0000

    - Changed dependency resolution
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1476 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-hfe/grails-app/conf/BuildConfig.groovy
+++ helio-hfe/grails-app/conf/BuildConfig.groovy
@@ -8,17 +8,17 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
 		mavenLocal()
-		mavenRepo "http://localhost:8081/archiva/repository/internal"
+		//mavenRepo "http://localhost:8081/archiva/repository/internal"
         grailsHome()
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/
+        mavenCentral()
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
