0af4646310e10a9afd2ddd2cef5a2e18159e800a
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-02-04 15:48:54 +0000

    Adding dummy HIT produces (A dummy for the CIS Server)
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1604 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-cis/helio-cis-client/src/main/java/org/helio/cis/hit/HelioIdentityTokenException.java
@@ -0,0 +1,11 @@
+package org.helio.cis.hit;
+
+public class HelioIdentityTokenException extends Exception 
+{
+	private static final long serialVersionUID = -3726351024692143790L;
+
+	public HelioIdentityTokenException(String string) 
+	{
+		super(string);
+	}
+}
---
+++ helio-cis/helio-cis-client/src/main/java/org/helio/cis/hit/HelioUserProfile.java
@@ -0,0 +1,6 @@
+package org.helio.cis.hit;
+
+public interface HelioUserProfile 
+{
+
+}
