77990b9462c38531c1125614f15168f9f4b77909
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-03-11 13:56:55 +0000

    remove hfe from svn
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1717 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-hfe/src/java/net/ivoa/xml/votable/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ivoa.net/xml/VOTable/v1.1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.ivoa.xml.votable.v1;
