25783a1de5ec7d11305e216a81aa5a5496142b60
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-03-28 21:26:27 +0000

    add buildinfo to war file
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1770 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-frontend/helio-portal/test/unit/ch/i4ds/helio/frontend/BuildInfoTest.java
@@ -0,0 +1,22 @@
+package ch.i4ds.helio.frontend;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class BuildInfoTest {
+	
+	@Test public void getProperties() {
+		BuildInfo instance = BuildInfo.getInstance();
+		assertNotNull(instance.getBuildID());
+		assertNotNull(instance.getBuildNumber());
+		assertNotNull(instance.getBuildTag());
+		assertNotNull(instance.getExecutorNumber());
+		assertNotNull(instance.getJavaHome());
+		assertNotNull(instance.getJenkinsUrl());
+		assertNotNull(instance.getJobName());
+		assertNotNull(instance.getSvnRevision());
+		assertNotNull(instance.getWorkspacePath());
+	}
+
+}
