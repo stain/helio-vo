beab4e3c736566f27e574732590f3929b4a68ed8
ddguevara <ddguevara@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-04-12 10:57:43 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1904 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-client/helio-clientapi/src/main/java/eu/heliovo/clientapi/frontend/SimpleInterface.java
+++ helio-client/helio-clientapi/src/main/java/eu/heliovo/clientapi/frontend/SimpleInterface.java
@@ -56,7 +56,7 @@ public class SimpleInterface {
 		int maxrecords = 1000;
 		int startindex = 0;
 		// timeout to wait for a response
-		int timeout = 30;
+		int timeout = 300;
 		HelioServiceType serviceType = SERVICE_TYPE;
 		
 		if (serviceName.equalsIgnoreCase("DPAS")) {
@@ -65,7 +65,7 @@ public class SimpleInterface {
 			startTime = normalizeList(numberOfFromSingles, startTime);
 			endTime = normalizeList(numberOfFromSingles, endTime);
 			from = normalizeList(numberOfDatePairs, from);
-			timeout = 49;
+			timeout = 300;
 		} else if (serviceName.equalsIgnoreCase("HEC")) {
 			serviceType = HelioServiceType.SYNC_QUERY_SERVICE;
 		}
