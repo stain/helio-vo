e184b5b11593ba7d63cde4aca17a37013c208560
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-04-19 14:45:08 +0000

    remove workflow from svn (not in use anymore)
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1923 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-workflow/src/main/java/eu/heliovo/workflow/clients/dpas/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://helio-vo.eu/xml/QueryService/v0.1")
-package eu.heliovo.workflow.clients.dpas;
--- helio-workflow/src/main/java/import-wsdl.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-del /Q eu\heliovo\workflow\clients\*.*
-wsimport -p eu.heliovo.workflow.clients.hec -keep -Xnocompile http://msslxw.mssl.ucl.ac.uk:8080/helio-hec/HelioTavernaService?wsdl
-wsimport -p eu.heliovo.workflow.clients.ics -keep -Xnocompile http://msslxw.mssl.ucl.ac.uk:8080/helio-ics/HelioTavernaService?wsdl
-wsimport -p eu.heliovo.workflow.clients.dpas -keep -Xnocompile http://msslxw.mssl.ucl.ac.uk:8080/helio-dpas/HelioTavernaService?wsdl
