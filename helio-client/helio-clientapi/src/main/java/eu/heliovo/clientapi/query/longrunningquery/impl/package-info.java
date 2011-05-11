22536c6481129ea5ab20e3b8752b11779d59de8c
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-05-11 21:24:04 +0000

    rename longrunning to async query
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1962 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-client/helio-clientapi/src/main/java/eu/heliovo/clientapi/query/longrunningquery/impl/package-info.java
+++
@@ -1,4 +0,0 @@
-package eu.heliovo.clientapi.query.longrunningquery.impl;
-/**
- * Decorator classes for the generated client stubs to access services that implement the long running query interface.
- */
