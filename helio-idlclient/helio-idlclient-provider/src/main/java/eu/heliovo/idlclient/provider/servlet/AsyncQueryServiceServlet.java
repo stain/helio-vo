e561ee34da77139bba4708fe2a5cfe849e64ac12
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-05-11 12:54:15 +0000

    Change error handling in helio-idlclient. Adapt to new helio-idlclient
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1955 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-idlclient/helio-idlclient-provider/src/main/java/eu/heliovo/idlclient/provider/servlet/AsyncQueryServiceServlet.java
+++ helio-idlclient/helio-idlclient-provider/src/main/java/eu/heliovo/idlclient/provider/servlet/AsyncQueryServiceServlet.java
@@ -5,17 +5,19 @@ import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import eu.heliovo.clientapi.query.HelioQueryResult;
 import eu.heliovo.clientapi.query.longrunningquery.AsyncQueryService;
-import eu.heliovo.clientapi.query.longrunningquery.impl.LongRunningQueryServiceFactory;
-import eu.heliovo.clientapi.registry.impl.LongRunningServiceDescriptor;
-import eu.heliovo.clientapi.workerservice.JobExecutionException;
+import eu.heliovo.clientapi.query.longrunningquery.impl.AsyncQueryServiceFactory;
 import eu.heliovo.idlclient.provider.serialize.IdlConverter;
+import eu.heliovo.shared.util.AssertUtil;
 
 /**
  * AsyncQueryServiceServlet for IDL Clients.
@@ -23,6 +25,11 @@ import eu.heliovo.idlclient.provider.serialize.IdlConverter;
  * Result is serialized for IDL and passed to IDL client.
  */
 public class AsyncQueryServiceServlet extends HttpServlet {
+    /**
+     * Happy little logger
+     */
+    private static final Logger _LOGGER = Logger.getLogger(AsyncQueryServiceServlet.class);
+    
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -30,27 +37,27 @@ public class AsyncQueryServiceServlet extends HttpServlet {
      */
     public AsyncQueryServiceServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/plain");
-		PrintWriter writer = response.getWriter();
-
-		String startTime = request.getParameter("starttime");
-		String endTime = request.getParameter("endtime");
-		String from = request.getParameter("from");
-		String service = request.getParameter("service");
-		String where = null;
 		
-		IdlHelioQueryResult idlresult = new IdlHelioQueryResult();
-		
-		//Check if all arguments exist
-		if(startTime != null && endTime != null && from != null && service != null)
-		{
+		try {
+    		String service = request.getParameter("service");
+    		String from = request.getParameter("from");
+    		String startTime = request.getParameter("starttime");
+    		String endTime = request.getParameter("endtime");
+    		String where = request.getParameter("where"); // the where clause, may be null
+    		
+    		AssertUtil.assertArgumentHasText(service, "service");
+    		AssertUtil.assertArgumentHasText(from, "from");
+    		AssertUtil.assertArgumentHasText(startTime, "startTime");
+    		AssertUtil.assertArgumentHasText(endTime, "endTime");
+    		
+    		IdlHelioQueryResult idlresult = new IdlHelioQueryResult();
+    		
 			//Split arguments to arrays
 			String[] startTimeArray = startTime.split(",");
 			String[] endTimeArray = endTime.split(",");
@@ -62,50 +69,19 @@ public class AsyncQueryServiceServlet extends HttpServlet {
 				for(int i=0; i < startTimeArray.length; ++i) df.parse(startTimeArray[i]);
 				for(int i=0; i < endTimeArray.length; ++i) df.parse(endTimeArray[i]);
 			} catch (ParseException e) {
-				RuntimeException e1 = new RuntimeException("Wrong date format. Date must be in ISO-8601 standard", e);
-				String out = IdlConverter.idlserialize(e1);
-				writer.append(out);
-				return;
-				//throw e1;
+			    throw new RuntimeException("Wrong date format. Date must be in ISO-8601 standard", e);
 			}
 			
-			LongRunningServiceDescriptor lrsd;
-			try{
-			lrsd =
-				LongRunningServiceDescriptor.valueOf("ASYNC_" + service.toUpperCase());
-			}catch (IllegalArgumentException e) {
-				RuntimeException e1 = new RuntimeException("Error, unknown service spezified");
-				String out = IdlConverter.idlserialize(e1);
-				writer.append(out);
-				return;
-				//throw e1;
-			}catch (NullPointerException e){
-				RuntimeException e1 = new RuntimeException("Error, no service spezified");
-				String out = IdlConverter.idlserialize(e1);
-				writer.append(out);
-				return;
-				//throw e1;
-			}
 			
-			LongRunningQueryServiceFactory queryServiceFactory = LongRunningQueryServiceFactory.getInstance();
-			AsyncQueryService queryService = queryServiceFactory.getLongRunningQueryService(lrsd);
+			AsyncQueryServiceFactory queryServiceFactory = AsyncQueryServiceFactory.getInstance();
+			AsyncQueryService queryService = queryServiceFactory.getAsyncQueryService(service.toUpperCase());
+			if (queryService == null) {
+			    throw new RuntimeException("Unable to find service with name " + service.toUpperCase());
+			}
+						
 			HelioQueryResult result;
 			
-			try{
-				result = queryService.query(Arrays.asList(startTimeArray), Arrays.asList(endTimeArray), Arrays.asList(fromArray), where, 100, 0, null);
-			}catch (JobExecutionException e){
-				RuntimeException e1 = new RuntimeException("Error, request timeout");
-				String out = IdlConverter.idlserialize(e1);
-				writer.append(out);
-				return;
-				//throw e1;
-			}catch (IllegalArgumentException e){
-				RuntimeException e1 = new RuntimeException("Error, wrong argument");
-				String out = IdlConverter.idlserialize(e1);
-				writer.append(out);
-				return;
-				//throw e1;
-			}
+			result = queryService.query(Arrays.asList(startTimeArray), Arrays.asList(endTimeArray), Arrays.asList(fromArray), where, 100, 0, null);
 			
 			if(result != null)
 			{
@@ -113,17 +89,17 @@ public class AsyncQueryServiceServlet extends HttpServlet {
 				idlresult.setLog(result.getUserLogs());
 				
 				String out = IdlConverter.idlserialize(idlresult);
+
+				PrintWriter writer = response.getWriter();
+		        response.setContentType("text/plain");
+		        response.setContentLength(out.length());
 				writer.append(out);
 			}
-		}
-		else
-		{
-			RuntimeException e1 = new RuntimeException("Error, missing arguments");
-			String out = IdlConverter.idlserialize(e1);
-			writer.append(out);
-			return;
-			//throw e1;
-		}
+		} catch (Exception e) {
+		    _LOGGER.warn("Exception while processing request: " + e.getMessage(), e);
+		    String out = IdlConverter.idlserialize(e);
+            response.sendError(500, out);
+        }
 	}
 
 	/**
