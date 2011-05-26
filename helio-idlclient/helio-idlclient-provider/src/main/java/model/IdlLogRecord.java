50fbbef6237f74821952b48f36d625ca8663e7d6
mmeyer7 <mmeyer7@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-05-26 12:18:33 +0000

    major changes and refactor of idlserializer
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@1979 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-idlclient/helio-idlclient-provider/src/main/java/model/IdlLogRecord.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.logging.LogRecord;
+
+public class IdlLogRecord {
+	
+	private final LogRecord logRecord;
+	
+	public IdlLogRecord(LogRecord logRecord)
+	{
+		this.logRecord = logRecord;
+	}
+	
+	public String getMessage()
+	{
+		return logRecord.getMessage();	
+	}
+	
+	public long getMillis()
+	{
+		return logRecord.getMillis();
+	}
+	
+	public long getSequenceNumber()
+	{
+		return logRecord.getSequenceNumber();
+	}
+	
+	public int getThreadID()
+	{
+		return logRecord.getThreadID();
+	}
+
+}
