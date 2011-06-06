79d157697e81f680916e25754e1ab2e0885dc314
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-06-06 04:47:38 +0000

    reduce ping time
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2011 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-monitoring/src/main/java/eu/heliovo/monitoring/serviceloader/RegistryClientServiceLoader.java
+++ helio-monitoring/src/main/java/eu/heliovo/monitoring/serviceloader/RegistryClientServiceLoader.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import eu.heliovo.monitoring.model.ModelFactory;
