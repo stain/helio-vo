805ff7c477535425697e556720e9235b73141b95
KevinBenson <KevinBenson@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-06-10 12:30:10 +0000

    checking in some php code that was in Bob Bentley Gui's.  Possibly it's been changed some already and need ot update them.
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2017 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-phpfrontend/php/helio-ics_soap6_pql.php
@@ -0,0 +1,275 @@
+<?php 
+//    some general parameters
+$qtype= $_GET["qtype"];
+
+$sql= $_GET["sql"];
+
+$cat= $_GET["cat1"];
+
+$format= $_GET["format"];
+if ($format == "") {$format='html';};
+$process= $_GET["process"];
+
+//  No of lines to output
+$n_show= $_GET['n_show'];
+if ($n_show == "") {$show = "";}
+else {$show = " limit " . $n_show;}
+
+//  Start Time
+$yy= $_GET["y_from"]; $mm= $_GET["mo_from"]; $dd= $_GET["d_from"]; 
+$hh= $_GET["h_from"]; $mn= $_GET["mi_from"]; $ss= $_GET["s_from"];
+$date_from= "$yy-$mm-$dd";
+$xx= "$hh:$mn:$ss";
+
+//  End Time
+$yy= $_GET["y_to"]; $mm= $_GET["mo_to"]; $dd= $_GET["d_to"]; 
+$hh= $_GET["h_to"]; $mn= $_GET["mi_to"]; $ss= $_GET["s_to"];
+$date_to= "$yy-$mm-$dd";
+$xx= "$hh:$mn:$ss";
+
+//  Instrument Type
+$itypx = $_GET['itype'];
+$itype= "";  $TOR= "";
+//if(itypx) $itype = "inst_type";
+foreach ($itypx as $itypz)
+{
+  if ($format == "text") {echo "$itypz is checked <br> \n";}
+  //if($itype) {$TOR= " OR ";}
+  //$itype= $itype . $TOR . "inst_type='" . $itypz . "'";
+  $itype .= "," . $itypz;
+}
+//if($itype) {$cl0 = " AND ($itype)"; } else {$cl0 = ""; }
+if($itype) {$cl0 = ";($itype)"; } else {$cl0 = ""; }
+if ($format == "text") {echo ">>> $cl0 <br> \n";}
+
+//  Observable Entity 1
+$oe1x = $_GET['oe1'];
+$oe1= "";  $TOR= "";
+//if ($oe1x) $oe1="inst_oe1";
+foreach ($oe1x as $oe1z)
+{
+  if ($format == "text") {echo "$oe1z is checked <br> \n";}
+  //if($oe1) {$TOR= " OR ";}
+  //$oe1= $oe1 . $TOR . "inst_oe1='" . $oe1z . "'";
+  $oe1 .= "," . $oe1z;
+}
+if($oe1) {$cl1 = ";($oe1)"; } else {$cl1 = ""; }
+if ($format == "text") {echo ">>> $cl1 <br> \n";}
+
+//  Observable Entity 2
+$oe2x = $_GET['oe2'];
+$oe2= "";  $TOR= "";
+//if ($oe2x) $oe2="inst_oe2";
+foreach ($oe2x as $oe2z)
+{
+  if ($format == "text") {echo "$oe2z is checked <br> \n";}
+  //if($oe2) {$TOR= " OR ";}
+  //$oe2= $oe2 . $TOR . "inst_oe2 LIKE '%" . $oe2z . "%'";
+  $oe2 .= ",*" . $oe2z . "*";
+}
+
+if($oe2) {$cl2 = ";($oe2)"; } else {$cl2 = ""; }
+if ($format == "text") {echo ">>> $cl2 <br> \n";}
+
+//  Observable Entity 2 additions
+//  need to or some stuff in from keywords...
+$oe2px = $_GET['oe2p'];
+$oe2p= "";  $TOR= "";
+//if($oe2px) $oe2p="keywords";
+foreach ($oe2px as $oe2pz)
+{
+  if ($format == "text") {echo "$oe2pz is checked <br> \n";}
+  //if($oe2) {$TOR= " OR ";}
+  //$oe2= $oe2 . $TOR . "keywords LIKE '%" . $oe2pz . "%'";
+  $oe2p .= ",*" . $oe2pz . "*";
+}
+//if($oe2) {$cl2 = " AND ($oe2)"; } else {$cl2 = ""; }
+if($oe2p) {$cl2 = ";($oe2)"; } else {$cl2 = ""; }
+if ($format == "text") {echo ">>> $cl2 <br> \n";}
+
+//  Observing Domain 1
+$od1x = $_GET['od1'];
+$od1= "";  $TOR= "";
+//if($od1x) $od1="inst_od1";
+foreach ($od1x as $od1z)
+{
+  if ($format == "text") {echo "$od1z is checked <br> \n";}
+  //if($od1) {$TOR= " OR ";}
+  //$od1= $od1 . $TOR . "inst_od1 LIKE '%" . $od1z . "%'";
+  $od1 .= ",*" . $od1z . "*";
+
+}
+//if($od1) {$cl4 = " AND ($od1)"; } else {$cl4 = ""; }
+if($od1) {$cl4 = ";($od1)"; } else {$cl4 = ""; }
+if ($format == "text") {echo ">>> $cl4 <br> \n";}
+
+//  Observing Domain 2
+$od2x = $_GET['od2'];
+$od2= "";  $TOR= "";
+//if($od1x) $od2 = "inst_od2";
+foreach ($od2x as $od2z)
+{
+  if ($format == "text") {echo "$od2z is checked <br> \n";}
+  //if($od2) {$TOR= " OR ";}
+  //$od2= $od2 . $TOR . "inst_od2 LIKE '%" . $od2z . "%'";
+  $od2 .= ",*" . $od2z . "*";
+}
+//if($od2) {$cl5 = " AND ($od2)"; } else {$cl5 = ""; }
+if($od2) {$cl5 = ";($od2)"; } else {$cl5 = ""; }
+
+if ($format == "text") {echo ">>> $cl5 <br> \n";}
+
+//  Keywords
+$keyx = $_GET['key'];
+$keys= "";  $TOR= "";
+//if($keyx) $keys = "keywords";
+foreach ($keyx as $keyz)
+{
+  if ($format == "text") {echo "$keyz is checked <br> \n";}
+  if($keys) {$TOR= " OR ";}
+  //$keys= $keys . $TOR . "keywords LIKE '%" . $keyz . "%'";
+  $keys .= ",*" . $keyz . "*";
+}
+//if($keys) {$cl3 = " AND ($keys)"; } else {$cl3 = ""; }
+if($keys) {$cl3 = ";($keys)"; } else {$cl3 = ""; }
+
+if ($format == "text") {echo ">>> $cl3 <br> \n";}
+
+if ($format == "text") {echo "<p> \n";}
+
+//  which parameters should be returned
+$cols = "name,observatory_name,obsinst_key,time_start,time_end,inst_type,inst_od1,inst_od2,inst_oe1,inst_oe2,keywords";
+$cols = "";
+if ($cat=='instrument') {
+  $cols = "obsinst_key,date(time_start),date(time_end),inst_od1,inst_od2,inst_type,inst_oe1,inst_oe2,keywords";
+}
+
+//  assemble the SQL string
+if($qtype) { 
+//$psql= "select * from " . $cat . " where time_start<='" . $date_from . "' and time_end>='" . $date_to . "'";
+//$psql= "select " . $cols . " from " . $cat . " where '" . $date_from . "'<=time_end and '" . $date_to . "'>=time_start";
+  $psql = "FROM="$cat;
+  if($date_from) {
+   $psql .= "&STARTTIME=" . $date_from;
+  }
+  if($date_to) {
+   $psql .= "&ENDTIME=" . $date_to;
+  }
+  if($cols) {
+   //enable the columns back a little later. KMB
+   //$psql .= "&SELECT=" . $cols;
+  }
+  if ($cat=='instrument') {
+     $psql .= "&WHERE=" . $cl0 . $cl1 . $cl2 . $cl4 . $cl5 . $cl3;
+  }
+//$psql=str_replace("%","%25",$psql);
+} else {
+$psql= $sql;
+$psql=str_replace("%","%25",$sql);
+};
+
+//  concatonate the URL
+//$root = 'http://msslxv.mssl.ucl.ac.uk:8080/stilts/task/sqlclient';
+$root = 'http://msslxw.mssl.ucl.ac.uk:8080/helio-ics/HelioQueryService';
+//$dtb  = 'jdbc:mysql://msslxt.mssl.ucl.ac.uk/helio_ics_ils&user=helio_guest';
+//$fmt  = 'html';
+//$fmt  = $format;
+$url  = '' . $root . '?' . $psql . '';
+if ($format == "text") {echo "$url \n";}
+
+
+//    output as text
+if ($format == "text") {
+
+// <!--   Do refresh ????   -->
+if($process) {
+echo '<meta http-equiv="REFRESH" content="0;url=' . "$url" . '">';
+} else {
+echo "<p> ++++ No Refresh! ++++ \n";
+}
+
+echo "<br> Query type: $qtype \n";
+echo "<br>List: $cat \n";
+echo "<br>Process: $process \n";
+
+//echo "<p> If necessary, <a href=\"" . $url . "\" target=\"_blank\"><b>follow this link</b></a>";
+echo "<p> If necessary, <a href=\"$url\" target=\"_blank\"> <b>follow this link</b> </a> \n";
+}
+
+//    define commend to retrieve output from ICS - depend on operating system
+$sys = strtoupper(PHP_OS);
+if ($format == "text") {echo "<p> Operating system: $sys <br>";}
+if ($sys == "DARWIN") {
+ $cget = 'curl -s "' . $url . '"';
+} else {
+ $cget = 'wget -qO- "' . $url . '"';
+}
+//echo "$cget <p>";
+//$cmd = $cget . ' | grep -e "sql> select" -e "Elapsed time:" -v' ;
+//$cmd = $cget . ' | sed -f stilts_modfmt.sed' ;
+
+$cmd = $cget . " | sed -f dpas_modfmt.sed | sed -f dpas_modfmt2.sed" ;
+
+if ($format == "text") {echo "<p> $cmd <p>";}
+
+//    define VOTable filename
+$dstr = date("ymd-His");
+$vot_file = "VOT-ICS_" . $dstr . ".xml";
+
+
+//    create a web page
+if ($format == "html") {
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 TRANSITIONAL//EN\"> \n";
+echo "<html> <head> \n";
+echo "<title>HELIO-ICS Results</title>\n";
+echo "</head> <body> \n";
+echo "<link rel=\"stylesheet\" href=\"secx.css\" type=\"text/css\"> \n";
+echo "<h1>HELIO ICS Results</h1> \n";  
+echo "<b>SQL Query:</b> &nbsp;&nbsp; $psql \n";
+echo "<p> \n";
+
+//    ask if want to save to file as a VOTable
+$nurl = str_replace('ofmt=html','ofmt=vot', $url);
+//    need to replace the & while passing or it transfer splits it into parameters
+$nurl = str_replace('&','||',$nurl);
+$nurl = str_replace("'","%27",$nurl);	// quotes on time can cause problems
+//echo "$nurl <p> \n";
+echo "Click to save to VOTable: <a href='stilts_savefile.php?file=$vot_file&urlx=$nurl'>$vot_file</a> <p>";
+
+//    this creates the table on the Web page
+echo passthru($cmd) ;
+
+echo "<p> \n";
+echo "If necessary, <a href=\"$url\" target=\"_blank\"> follow this link </a> \n";
+echo "<p> \n";
+
+echo "</body> </html> \n";
+}
+
+//    output to VOTable
+if ($format == "vot") {
+
+header("Content-type: application/x-file-to-save");
+header("Content-Disposition: attachment; filename=".$vot_file);
+readfile($vot_file);
+
+//$cmd = 'curl -s "' . $url . '" | grep -e "sql> select" -e "Elapsed time:" -v' ;
+echo passthru($cmd) ;
+}
+
+//    output is in CSV format
+if ($format == "csv") {
+
+//header("Content-type: application/x-file-to-save");
+//header("Content-Disposition: attachment; filename=".$vot_file);
+//readfile($vot_file);
+
+//$cmd = 'curl -s "' . $url . '" | grep -e "sql> select" -e "Elapsed time:" -v' ;
+
+echo "<pre>";
+echo passthru($cmd) ;
+echo "</pre> \n";
+
+}
+
+?>
---
+++ helio-phpfrontend/php/helio-ics_ui6_pql.php
@@ -0,0 +1,362 @@
+<html>
+ <head>
+  <title>HELIO ICS - Test GUI</title>
+  <link rel="stylesheet" href="secr.css" type="text/css">
+  <meta content="">
+  <style></style>
+  <link rel="SHORTCUT ICON" href="../../logos/helio_icon.gif">
+ </head>
+ <body bgcolor="white">
+  <table  bgcolor="white" border="0" cellpadding="2" cellspacing="0" height="100" width="750">
+   <tbody>
+    <tr>
+     <td valign="middle" align="left" width="170">
+      <a href="http://www.helio-vo.eu/"><img src="../../logos/helio_logo4_sm.jpg" border="0"></a>
+     </td>
+     <td valign="middle" align="left" width="480">
+      <font size=+2 color=red><b>
+        Instrument Capabilities Service (ICS)<br>Test GUI
+       </b>
+      </font>
+     </td>
+     </td>
+     <td valign="middle" align="right" width="100">
+      <a href="http://ec.europa.eu/research/fp7/index_en.cfm?pg=capacities">
+       <img src="../../images/FP7-cap-RGB12.gif" border="0">
+      </a>
+     </td>
+    </tr>
+   </tbody>
+  </table>
+
+<?php
+include("datetime_functions.php");
+
+include("default_interval.php");	// default time interval for GUI
+
+$url = "helio-ics_soap6_pql.php";		// routine that will process the form
+?>
+  <hr>
+
+<!--        Preset Query           -->
+  <p></p><h3>Preset Search</h3>
+  <form method="get" action="<?php echo $url ?>" target="_blank">
+   <input name="qtype" value="1" type="hidden">
+   <p>Select a List: &nbsp;&nbsp;
+    <select name="cat1">
+     <option value="instrument">Instrument</option>
+     <option value="observatory">Observatory</option>
+     <option disabled="disabled">---------</option>
+     <option value="flybys">Flybys</option>
+    </select>
+   </p>
+   <p>
+    <table cellspacing=0 cellpadding=0>
+     <tbody>
+      <tr>
+       <td><p>Starting date:</p></td>
+       <td>&nbsp;&nbsp;
+         <?php 
+         echo date_picker("from", $from_tint_yr, $from_tint_mo, $from_tini_dy);
+//         echo "&nbsp;&nbsp; \n";
+//         echo time_picker("from");
+         ?>
+       </td>
+      </tr>
+      <tr>
+       <td>Ending date:</td>
+       <td>&nbsp;&nbsp;
+         <?php
+ //        $curr_time = time();
+ //        $cyr = date('Y',$curr_time);
+ //        $cmo = date('n',$curr_time);
+ //        $cdy = date('j',$curr_time);
+         //echo "$cyr - $cmo - $cdy <p> \n";
+         echo date_picker("to", $to_tint_yr, $to_tint_mo, $to_tini_dy);  //, $cyr, $cmo, $cdy);
+//         echo "&nbsp;&nbsp; \n";
+//         echo time_picker("to", 23, 59, 59);
+         ?>
+       </td>
+      </tr>
+     </tbody>
+    </table>
+<!--            -->
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    <i>Show Recs.:</i> &nbsp;&nbsp;&nbsp;&nbsp;
+    <select name="n_show">
+     <option value="" selected="selected">all</option>
+     <option value="25">25</option>
+     <option value="50">50</option>
+     <option value="75">75</option>
+     <option value="100">100</option>
+    </select>
+    <br>
+<!--               -->
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <input value=" &nbsp; Search &nbsp; " type="submit" style="background-color:lightgreen; color:black;">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input value="Reset" type="reset" style="background-color:#FFE065; color:black;">
+<!--               -->
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    <select name="format">
+     <option value="text">Text</option>
+     <option value="html" selected="selected">HTML</option>
+     <option value="vot">VOTable</option>
+     <option value="csv">CSV File</option>
+    </select>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <select name="process">
+     <option value="1">Auto</option>
+     <option value="0">Hold</option>
+    </select>
+   </p>
+
+<!--               -->
+   <p> <br>
+   <b>Qualifying Parameters:</b> &nbsp; <i>(for Instrument)</i>
+   <p>
+<!--    -->
+   <i>Observing Domain 1</i>: <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   Sun
+   <input name="od1[]" value="Sun" type="checkbox"></input> &nbsp;&nbsp;
+   Mercury
+   <input name="od1[]" value="Mercury" type="checkbox"></input> &nbsp;&nbsp;
+   Venus
+   <input name="od1[]" value="Venus" type="checkbox"></input> &nbsp;&nbsp;
+   Earth
+   <input name="od1[]" value="Earth" type="checkbox"></input> &nbsp;&nbsp;
+<!--
+Earth/L1 <input name="od1[]" value="Earth/L1" type="checkbox"></input> &nbsp;&nbsp;
+-->
+   Mars
+   <input name="od1[]" value="Mars" type="checkbox"></input> &nbsp;&nbsp;
+   Jupiter
+   <input name="od1[]" value="Jupiter" type="checkbox"></input> &nbsp;&nbsp;
+   Saturn
+   <input name="od1[]" value="Saturn" type="checkbox"></input> &nbsp;&nbsp;
+   <br>
+<!--    -->
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   Heliosphere
+   <input name="od1[]" value="heliosphere" type="checkbox"></input> &nbsp;&nbsp;
+   Planetary
+   <input name="od1[]" value="planetary" type="checkbox"></input> &nbsp;&nbsp;
+   Comet
+   <input name="od1[]" value="comet" type="checkbox"></input> &nbsp;&nbsp;
+   Heliopause
+   <input name="od1[]" value="heliopause" type="checkbox"></input> &nbsp;&nbsp;
+   Galactic
+   <input name="od1[]" value="galactic" type="checkbox"></input> &nbsp;&nbsp;
+   <br>
+<!--    -->
+   <i>Observing Domain 2</i>: <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   <i>Solar</i>: &nbsp;&nbsp;
+   Interior
+   <input name="od2[]" value="interior" type="checkbox"></input> &nbsp;&nbsp;
+<!--
+Disk <input name="od2[]" value="disk" type="checkbox"></input> &nbsp;&nbsp-->
+   Disk/inr. cor.
+   <input name="od2[]" value="disk/inr. cor." type="checkbox"></input> &nbsp;&nbsp;
+   Outer corona
+   <input name="od2[]" value="outer corona" type="checkbox"></input> &nbsp;&nbsp;
+   Disk/helios.
+   <input name="od2[]" value="disk/helios." type="checkbox"></input> &nbsp;&nbsp;
+   Solar-wind
+   <input name="od2[]" value="solar-wind" type="checkbox"></input> &nbsp;&nbsp;
+   <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   <i>Planetary</i>: &nbsp;&nbsp;
+   Environment
+   <input name="od2[]" value="environment" type="checkbox"></input> &nbsp;&nbsp;
+   Magnetosphere
+   <input name="od2[]" value="magneto" type="checkbox"></input> &nbsp;&nbsp;
+<!--
+Magneto/ionosphere <input name="od2[]" value="magneto/ionosphere" type="checkbox"></input> &nbsp;&nbsp;
+-->
+   Ionosphere
+   <input name="od2[]" value="ionosphere" type="checkbox"></input> &nbsp;&nbsp;
+   Aurora
+   <input name="od2[]" value="aurora" type="checkbox"></input> &nbsp;&nbsp;
+   <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   Interstellar
+   <input name="od2[]" value="interstellar" type="checkbox"></input> &nbsp;&nbsp;
+   Energy release
+   <input name="od2[]" value="energy release" type="checkbox"></input> &nbsp;&nbsp;
+   Structure
+   <input name="od2[]" value="structure" type="checkbox"></input> &nbsp;&nbsp;
+   <p>
+<!--    -->
+   <i>Instrument Type</i>: <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   Remote
+   <input name="itype[]" value="remote" type="checkbox"></input> &nbsp;&nbsp;
+   In-situ
+   <input name="itype[]" value="in-situ" type="checkbox"></input> &nbsp;&nbsp;
+   <br>
+<!--    -->
+   <i>Observable Entity</i>: <br>
+   <table cellpadding=0 cellspacing=0>
+    <tr>
+     <td width="150">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      Photons
+      <input name="oe1[]" value="photons" type="checkbox"></input>
+     </td>
+     <td>
+      GMR
+      <input name="oe2[]" value="gmr" type="checkbox"></input> &nbsp;&nbsp;
+      HXR
+      <input name="oe2[]" value="hxr" type="checkbox"></input> &nbsp;&nbsp;
+      SXR
+      <input name="oe2[]" value="sxr" type="checkbox"></input> &nbsp;&nbsp;
+      EUV
+      <input name="oe2[]" value="euv" type="checkbox"></input> &nbsp;&nbsp;
+      UV
+      <input name="oe2[]" value="uv" type="checkbox"></input> &nbsp;&nbsp;
+      visible
+      <input name="oe2[]" value="visible" type="checkbox"></input> &nbsp;&nbsp;
+      &mu;-wave
+      <input name="oe2[]" value="microwave" type="checkbox"></input> &nbsp;&nbsp;
+      radio
+      <input name="oe2[]" value="radio" type="checkbox"></input> &nbsp;&nbsp;
+      <br>
+      H-alpha
+      <input name="oe2p[]" value="H-alpha" type="checkbox"></input> &nbsp;&nbsp;
+      He 10830
+      <input name="oe2p[]" value="He10830" type="checkbox"></input> &nbsp;&nbsp;
+     </td>
+    </tr>
+    <tr>
+     <td width="150">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      Particles
+      <input name="oe1[]" value="particles" type="checkbox"></input>
+     </td>
+     <td>
+      Charged
+      <input name="oe2[]" value="charged" type="checkbox"></input> &nbsp;&nbsp;
+      Energetic
+      <input name="oe2[]" value="energ" type="checkbox"></input> &nbsp;&nbsp;
+<!--
+energ/neut <input name="oe2[]" value="energ/neut" type="checkbox"></input> &nbsp;&nbsp;
+-->
+      Neutral
+      <input name="oe2[]" value="neut" type="checkbox"></input> &nbsp;&nbsp;
+      Dust
+      <input name="oe2[]" value="dust" type="checkbox"></input> &nbsp;&nbsp;
+      Cosmic-ray
+      <input name="oe2p[]" value="cosmic-ray" type="checkbox"></input> &nbsp;&nbsp;
+     </td>
+    </tr>
+    <tr>
+     <td width="150">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      Fields
+      <input name="oe1[]" value="fields" type="checkbox"></input>
+     </td>
+     <td>
+      Electric
+      <input name="oe2[]" value="elect" type="checkbox"></input> &nbsp;&nbsp;
+      Magnetic
+      <input name="oe2[]" value="magn" type="checkbox"></input> &nbsp;&nbsp;
+<!--
+elect/magn. <input name="oe2[]" value="uv" type="checkbox"></input> &nbsp;&nbsp;
+-->
+      Gravity
+      <input name="oe2[]" value="gravity" type="checkbox"></input> &nbsp;&nbsp;
+     </td>
+    </tr>
+   </table>
+   <p>
+<!--    -->
+<!--
+dust    ?????
+-->
+<!--    -->
+   <i>Keywords</i>: <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   Imager
+   <input name="key[]" value="imager" type="checkbox"></input> &nbsp;&nbsp;
+   Spectrometer
+   <input name="key[]" value="spectrometer" type="checkbox"></input> &nbsp;&nbsp;
+   Polarimeter
+   <input name="key[]" value="polarimeter" type="checkbox"></input> &nbsp;&nbsp;
+   Coronagraph
+   <input name="key[]" value="coronagraph" type="checkbox"></input> &nbsp;&nbsp;
+   Magnetograph
+   <input name="key[]" value="magnetograph" type="checkbox"></input> &nbsp;&nbsp;
+   Magnetometer
+   <input name="key[]" value="magnetometer" type="checkbox"></input> &nbsp;&nbsp;
+   <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<!--
+Radioheliograph <input name="key[]" value="radioheliograph" type="checkbox"></input> &nbsp;&nbsp;
+-->
+   Oscillations
+   <input name="key[]" value="oscillations" type="checkbox"></input> &nbsp;&nbsp;
+   Composition
+   <input name="key[]" value="composition" type="checkbox"></input> &nbsp;&nbsp;
+   Irradiance
+   <input name="key[]" value="irradiance" type="checkbox"></input> &nbsp;&nbsp;
+   Photometer
+   <input name="key[]" value="photometer" type="checkbox"></input> &nbsp;&nbsp;
+   Radiometer
+   <input name="key[]" value="radiometer" type="checkbox"></input> &nbsp;&nbsp;
+  </form>
+  </p>
+  <hr>
+<!--        Free SQL Query           -->
+  <p></p><h3>Free SQL Query</h3>
+  <form method="get" action="<?php echo $url ?>" target="_blank">
+   <input name="qtype" value="0" type="hidden">
+   <p>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   <textarea name="sql" cols="75" rows="8">SELECT name,loc_gen,loc_p1,loc_p2,sat_id FROM observatory WHERE loc_gen='ERO' OR loc_gen='LPO' LIMIT 100
+   </textarea>
+<!--               -->
+   <p>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <input value=" &nbsp; Search &nbsp; " type="submit" style="background-color:lightgreen; color:black;">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input value="Reset" type="reset" style="background-color:#FFE065; color:black;">
+<!--               -->
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+   <select name="format">
+    <option value="text">Text</option>
+    <option value="html">HTML</option>
+    <option value="vot">VOTable</option>
+    <option value="csv">CSV File</option>
+   </select>
+   &nbsp;&nbsp;&nbsp;&nbsp;
+   <select name="process">
+    <option value="1">Auto</option>
+    <option value="0">Hold</option>
+   </select>
+  </form>
+  <br>
+  </p>
+  <p>
+  <a href="examples_ics-ils.html" target=_blank"><i>Examples of SQL Queries to the ICS and ILS</i></a>
+   </p>
+<!--
+   <p>
+    <a href="http://hec.ts.astro.it/sec_idiots_sql.html" target="_blank">
+     <i>Examples of how to use SQL on the HELIO HEC Server</i>
+    </a>
+   </p>
+
+-->
+   <hr>
+   <?php include("ucl_inaf-service_ack.html"); ?>
+ </body>
+</html>
