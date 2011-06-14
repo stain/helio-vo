90428568c54a5e9b5e328184380565d7dd27a2ba
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-06-14 16:52:09 +0000

    Complete - task : Set Preferences
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2019 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-cis/helio-cis-server/src/test/java/eu/heliovo/cis/service/CisServiceTest.java
@@ -0,0 +1,314 @@
+package eu.heliovo.cis.service;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import eu.heliovo.shared.common.cis.hit.HIT;
+import eu.heliovo.shared.common.cis.hit.HITPayload;
+import eu.heliovo.shared.common.cis.hit.HITUtilities;
+import eu.heliovo.shared.common.cis.hit.HITUtilitiesException;
+import eu.heliovo.shared.common.cis.hit.info.HITInfoException;
+import eu.heliovo.shared.common.cis.hit.preferences.PreferencesUtilities;
+import eu.heliovo.shared.common.cis.tags.HELIOTags;
+import eu.heliovo.shared.common.utilities.ClassAdUtilitiesException;
+import eu.heliovo.shared.common.utilities.SecurityUtilities;
+import eu.heliovo.shared.common.utilities.SecurityUtilitiesException;
+
+public class CisServiceTest 
+{
+	HITUtilities			hitUtilities	=	new HITUtilities();	
+	CisService				cisInterface	=	new CisServiceImpl();
+	SecurityUtilities		secUtils		=	new SecurityUtilities();
+	PreferencesUtilities	prUtilities		=	new PreferencesUtilities();
+
+	
+	@Test
+	public void testUserCreation() 
+	{
+		String 	user	=	"test_user";
+		String	pwd		=	"pwd_for_test_user";
+		
+		try 
+		{
+			/*
+			 * If the test user is present, remove it
+			 */
+			if(cisInterface.isUserPresent(user))
+			{
+				/*
+				 * Validates the user and, if valid removes it
+				 */
+				if(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)))
+				{
+					cisInterface.removeUser(user, secUtils.computeHashOf(pwd));
+					assertFalse(cisInterface.isUserPresent(user));
+				}
+			}
+			/*
+			 * Now, add the user
+			 */
+			cisInterface.addUser(user, secUtils.computeHashOf(pwd));
+			assertTrue(cisInterface.isUserPresent(user));
+			assertTrue(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)));
+		} 
+		catch (CisServiceException e1) 
+		{
+			e1.printStackTrace();
+		} 
+		catch (SecurityUtilitiesException e1) 
+		{
+			e1.printStackTrace();
+		}
+	}
+	
+
+	@Test
+	public void testUserPreferences() 
+	{
+		String 	user	=	"test_user";
+		String	pwd		=	"pwd_for_test_user";
+		
+		try 
+		{
+			/*
+			 * If the test user is present, remove it
+			 */
+			if(cisInterface.isUserPresent(user))
+			{
+				/*
+				 * Validates the user and, if valid removes it
+				 */
+				if(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)))
+				{
+					cisInterface.removeUser(user, secUtils.computeHashOf(pwd));
+					assertFalse(cisInterface.isUserPresent(user));
+				}
+			}
+			/*
+			 * Now, add the user
+			 */
+			cisInterface.addUser(user, secUtils.computeHashOf(pwd));
+			assertTrue(cisInterface.isUserPresent(user));
+			assertTrue(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)));
+			/*
+			 * Retrieve the preferences for the following service
+			 * Service : dpas
+			 * Element : dpas_data_providers
+			 */
+			HITPayload	hitPayload	=	 cisInterface.getLowSecurityHITFor(user, secUtils.computeHashOf(pwd));
+			System.out.println(hitPayload.getInformation());
+			HIT	hit	= 	hitUtilities.buildHITFromPayload(hitPayload);
+			
+			System.out.println(hit);
+			System.out.println(prUtilities.getElementFor(
+					hit.getHitInfo().getProfile(), 
+					HELIOTags.dpas, 
+					HELIOTags.dpas_data_providers));
+			/*
+			 * Setting the preferences
+			 */
+			String preferences	= "provider3, provider2, provider1";
+			
+			cisInterface.setStringPreferencesFor(user, 
+					secUtils.computeHashOf(pwd), 
+					HELIOTags.dpas, 
+					HELIOTags.dpas_data_providers, 
+					preferences);
+
+			hitPayload	=	 cisInterface.getLowSecurityHITFor(user, secUtils.computeHashOf(pwd));
+			System.out.println(hitPayload.getInformation());
+			hit	= 	hitUtilities.buildHITFromPayload(hitPayload);
+			
+			System.out.println(prUtilities.getElementFor(
+					hit.getHitInfo().getProfile(), 
+					HELIOTags.dpas, 
+					HELIOTags.dpas_data_providers));
+
+			assertTrue(prUtilities.getElementFor(
+					hit.getHitInfo().getProfile(), 
+					HELIOTags.dpas, 
+					HELIOTags.dpas_data_providers).equals(preferences));			
+		} 
+		catch (CisServiceException e1) 
+		{
+			e1.printStackTrace();
+		} 
+		catch (SecurityUtilitiesException e1) 
+		{
+			e1.printStackTrace();
+		} 
+		catch (HITUtilitiesException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (HITInfoException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (ClassAdUtilitiesException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	
+	
+//	@Test
+//	public void testValidateUser() 
+//	{
+//		String 	user	=	"user_a";
+//		String	pwd		=	"pwd_for_user_a";
+//		
+//		try 
+//		{
+//			if(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)))
+//				System.out.println(user + " is authorized !");
+//			else
+//				System.out.println(user + " is NOT authorized !");				
+//		} 
+//		catch (Exception e) 
+//		{
+//			e.printStackTrace();
+//			fail("Exception raised !");
+//		}
+//		
+//		try 
+//		{
+//			cisInterface.addUser(user, secUtils.computeHashOf(pwd));
+//		} 
+//		catch (Exception e1) 
+//		{
+//			System.out.println("Cannot add " + user + ", it is probably already present");
+//		}
+//
+//		try 
+//		{
+//			if(cisInterface.validateUser(user, secUtils.computeHashOf("pwd_for_user_a")))
+//				System.out.println(user + " is authorized !");
+//			else
+//				System.out.println(user + " is NOT authorized !");				
+//		} 
+//		catch (Exception e) 
+//		{
+//			e.printStackTrace();
+//			fail("Exception raised !");
+//		}
+//		
+//		try 
+//		{
+//			System.out.println("HIT for " + user + cisInterface.getLowSecurityHITFor("user_a", secUtils.computeHashOf(pwd)));
+//		} 
+//		catch (Exception e) 
+//		{
+//			e.printStackTrace();
+//			fail("Exception raised !");
+//		}
+//	}
+//	
+//	@Test
+//	public void testUserPreferences() 
+//	{
+//		String 	user	=	"user_a";
+//		String	pwd		=	"pwd_for_user_a";
+//			
+//	try 
+//	{
+//		if(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)))
+//			System.out.println(user + " is authorized !");
+//		else
+//			System.out.println(user + " is NOT authorized !");				
+//	} 
+//	catch (Exception e) 
+//	{
+//		e.printStackTrace();
+//		fail("Exception raised !");
+//	}
+//	
+//	try 
+//	{
+//		if(cisInterface.isUserPresent(user))
+//			System.out.println(user + " is already present, cannot add it !");
+//		else
+//		{
+//			System.out.println("Adding user " + user + " ...");
+//			cisInterface.addUser(user, secUtils.computeHashOf(pwd));
+//			System.out.println("... done !");
+//		}
+//	} 
+//	catch (CisServiceException e1) 
+//	{
+//		e1.printStackTrace();
+//	} 
+//	catch (SecurityUtilitiesException e) 
+//	{
+//		e.printStackTrace();
+//	}
+//
+//	try 
+//	{
+//		if(cisInterface.validateUser(user, secUtils.computeHashOf(pwd)))
+//			System.out.println(user + " is authorized !");
+//		else
+//			System.out.println(user + " is NOT authorized !");				
+//	} 
+//	catch (Exception e) 
+//	{
+//		e.printStackTrace();
+//		fail("Exception raised !");
+//	}
+//	
+//
+//	try 
+//	{
+//		HITPayload	hitPayload	=	 cisInterface.getLowSecurityHITFor(user, secUtils.computeHashOf(pwd));
+//		System.out.println(hitPayload.getInformation());
+//		HIT	hit	= 	hitUtilities.buildHITFromPayload(hitPayload);
+//		
+//		System.out.println(hit);
+//		System.out.println(prUtilities.getElementFor(
+//				hit.getHitInfo().getProfile(), 
+//				HELIOTags.dpas, 
+//				HELIOTags.dpas_data_providers));
+//	} 
+//	catch (Exception e) 
+//	{
+//		e.printStackTrace();
+//		fail("Exception raised !");
+//	}
+//	
+//	try 
+//	{
+//		cisInterface.setStringPreferencesFor(user, 
+//				secUtils.computeHashOf(pwd), 
+//				HELIOTags.dpas, 
+//				HELIOTags.dpas_data_providers, 
+//				"provider3, provider2, provider1");
+//	} 
+//	catch (Exception e) 
+//	{
+//		e.printStackTrace();
+//	}
+//	try 
+//	{
+//		HITPayload	hitPayload	=	 cisInterface.getLowSecurityHITFor(user, secUtils.computeHashOf(pwd));
+//		System.out.println(hitPayload.getInformation());
+//		HIT	hit	= 	hitUtilities.buildHITFromPayload(hitPayload);
+//		
+//		System.out.println(hit);
+//		System.out.println(prUtilities.getElementFor(
+//				hit.getHitInfo().getProfile(), 
+//				HELIOTags.dpas, 
+//				HELIOTags.dpas_data_providers));
+//	} 
+//	catch (Exception e) 
+//	{
+//		e.printStackTrace();
+//		fail("Exception raised !");
+//	}
+//
+//	}
+	
+	
+}
