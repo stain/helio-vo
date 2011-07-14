3e4694ccfca471fbe2b2cb93067e354625333bfd
vineethtshetty <vineethtshetty@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-07-14 10:54:04 +0000

    Changing UOC folder structure to HQI
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2143 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/service/eu/helio_vo/xml/queryservice/v0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://helio-vo.eu/xml/QueryService/v0.1")
-package eu.heliovo.dpas.ie.services.uoc.service.eu.helio_vo.xml.queryservice.v0;
--- helio-dpas/src/main/java/eu/heliovo/dpas/ie/services/uoc/service/net/ivoa/xml/votable/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ivoa.net/xml/VOTable/v1.1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package eu.heliovo.dpas.ie.services.uoc.service.net.ivoa.xml.votable.v1;
