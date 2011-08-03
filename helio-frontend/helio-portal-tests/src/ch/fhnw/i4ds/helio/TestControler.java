76be6701a25d086c74ca2ed07872727400de762b
l-lavanchy <l-lavanchy@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-08-03 06:15:11 +0000

    First test, with Andrés Workflow. Basic in Selenium for the future
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2163 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-frontend/helio-portal-tests/src/ch/fhnw/i4ds/helio/TestControler.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ch.fhnw.i4ds.helio;
+
+/**
+ * @author Lavanchy
+ *
+ */
+public class TestControler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("test is runing ");
+				
+		AndresTest1 andre1 = new AndresTest1();
+		andre1.runFF();
+		//andre1.runIE();
+		
+
+	}
+
+}
