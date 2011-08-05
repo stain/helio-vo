def16b0511f95f97eb60b06711214d8c32270cbf
l-lavanchy <l-lavanchy@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-08-05 14:38:01 +0000

    TestControler run the test with a portable Firefox with is included in the project.
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2168 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-frontend/helio-portal-tests/src/ch/fhnw/i4ds/helio/TestControler.java
+++ helio-frontend/helio-portal-tests/src/ch/fhnw/i4ds/helio/TestControler.java
@@ -3,25 +3,42 @@
  */
 package ch.fhnw.i4ds.helio;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * @author Lavanchy
- *
+ * 
  */
 public class TestControler {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
 	public static void main(String[] args) {
-	
-		System.out.println("test is runing ");
-				
-		AndresTest1 andre1 = new AndresTest1();
-		//andre1.runFF();
-		andre1.runChrom();
-		//andre1.runIE();
+
+		String urlDev = "http://helio.i4ds.technik.fhnw.ch/Helio-dev/prototype/explorer";
+		String tag = "Test controller info: ";
+		System.out.println(tag + "test is runing ");
 		
 
+		// Sel 1
+		// AndresTest1 andre1 = new AndresTest1();
+		// andre1.runFF();
+		// andre1.runChrom();
+		// andre1.runIE();
+		try{
+			System.setProperty("webdriver.firefox.bin",
+			"PortableBrowser/Portable_Firefox_4.0/FirefoxLoader.exe");	
+		andreSel2 andre2 = new andreSel2(urlDev);
+		WebDriver driver = new FirefoxDriver();
+		andre2.runTests(driver);
+		}catch (Exception e) {
+			System.out.println(tag + "andreSel2 couldn’t be finished. ");
+		}
+
+		System.out.println(tag + "End of all tests");
 	}
 
 }
