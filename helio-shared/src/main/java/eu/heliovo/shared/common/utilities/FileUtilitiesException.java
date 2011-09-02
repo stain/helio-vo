876e3ad5ead3671b2b45dbad37d515de66e432b7
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-09-02 17:58:18 +0000

    Simple File Utilities used by CIS, HPS and HSS
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2199 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-shared/src/main/java/eu/heliovo/shared/common/utilities/FileUtilitiesException.java
@@ -0,0 +1,10 @@
+package eu.heliovo.shared.common.utilities;
+
+public class FileUtilitiesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2167710506116773393L;
+
+}
