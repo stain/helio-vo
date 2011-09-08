5621f41e5979a2c8a60f964e6b1f3966a03d60e6
dkf <dkf@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-09-08 09:59:41 +0000

    Starting to add registry integration
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2223 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-taverna-interface/src/main/java/eu/heliovo/myexperiment/helio/Helio.java
+++ helio-taverna-interface/src/main/java/eu/heliovo/myexperiment/helio/Helio.java
@@ -8,11 +8,6 @@ import org.xml.sax.SAXException;
 
 import eu.heliovo.myexperiment.Group;
 import eu.heliovo.myexperiment.Repository;
-import eu.heliovo.registryclient.AccessInterface;
-import eu.heliovo.registryclient.AccessInterfaceType;
-import eu.heliovo.registryclient.HelioServiceName;
-import eu.heliovo.registryclient.ServiceRegistryClient;
-import eu.heliovo.registryclient.impl.ServiceRegistryClientFactory;
 
 /**
  * Access to HELIO-specific things on myExperiment.
@@ -25,22 +20,11 @@ public class Helio {
 
 	private static Repository myExperiment;
 	private static Group helio;
-	public static final HelioServiceName REGISTRY_KEY = HelioServiceName
-			.register("myExperiment", null);
-	public static final AccessInterfaceType myExpAPI = AccessInterfaceType
-			.register("myExperiment_REST", "http://rest");
 	public static final String GROUP_NAME = "helio";
 
 	static {
 		try {
-			ServiceRegistryClient registry = ServiceRegistryClientFactory
-					.getInstance().getServiceRegistryClient();
-			for (AccessInterface ai : registry.getAllEndpoints(
-					registry.getServiceDescriptor(REGISTRY_KEY), null, null))
-				if (myExpAPI.equals(ai.getInterfaceType())) {
-					myExperiment = new Repository(ai.getUrl().toString());
-					break;
-				}
+			myExperiment = new Repository();
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		}
@@ -54,6 +38,9 @@ public class Helio {
 	 *             If things are unaccessible.
 	 * @throws SAXException
 	 *             If myExperiment produces bad XML.
+	 * @deprecated Use
+	 *             {@link eu.heliovo.tavernaserver.util.Registry#getHelioGroup()}
+	 *             instead.
 	 */
 	public static Group group() throws IOException, SAXException {
 		if (helio == null)
