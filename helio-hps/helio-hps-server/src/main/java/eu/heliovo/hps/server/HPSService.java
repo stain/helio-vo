0e10f3f85b173d3a6f5079630c392a8b121556d1
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-09-12 21:41:37 +0000

    Incomplete - task : Clean and Commit Code
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2266 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-hps/helio-hps-server/src/main/java/eu/heliovo/hps/server/HPSService.java
@@ -0,0 +1,51 @@
+package eu.heliovo.hps.server; 
+
+import java.util.Collection;
+import java.util.Vector;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+
+@WebService
+public interface HPSService 
+{
+	/**
+	 * Test
+	 *
+	 * @param a string parameter
+	 */
+	@WebMethod
+	@WebResult(name="testResult")
+	public	String	test(@WebParam(name="testParameter") String testParemeter);
+
+//	/**
+//	 * getPresentApplications
+//	 */
+//	@WebMethod
+//	public	Collection<ApplicationDescription> getPresentApplications();
+//	
+//	@WebMethod
+//	public String getStatusOfExecution(String exeId);
+//
+//	@WebMethod
+//	public Boolean isRunning(String exeId);
+//
+//	@WebMethod
+//	public void putFile(String fileLocation, String fileContent) throws HPSServiceException;
+//
+//	@WebMethod
+//	public String getFile(String fileLocation) throws HPSServiceException;
+//
+//	@WebMethod
+//	public String executeApplication(
+//			ApplicationDescription app,
+//			Boolean fastExecution, 
+//			int numParallelJobs) throws ApplicationEngineException;
+//
+//	@WebMethod
+//	public String getOutputOfExecution(String exeId) throws HPSServiceException;
+
+}
---
+++ helio-hps/helio-hps-server/src/main/java/eu/heliovo/hps/server/HPSServiceImpl.java
@@ -0,0 +1,10 @@
+package eu.heliovo.hps.server;
+
+public class HPSServiceImpl implements HPSService 
+{
+	@Override
+	public String test(String arg) 
+	{
+		return arg;
+	}	
+}
