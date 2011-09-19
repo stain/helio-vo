f917dbe81d52bc0368d2406bf4a41a470c24f334
l-lavanchy <l-lavanchy@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-09-19 12:44:22 +0000

    Andres workflow with HXR works properly
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2288 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-frontend/helio-portal-tests/src/ch/fhnw/i4ds/helio/andreSel2.java
+++ helio-frontend/helio-portal-tests/src/ch/fhnw/i4ds/helio/andreSel2.java
@@ -1,21 +1,28 @@
 package ch.fhnw.i4ds.helio;
 
 import static org.junit.Assert.fail;
-
-import org.apache.xalan.xsltc.compiler.sym;
-import org.junit.After;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+/**
+ * 
+ * @author Lavanchy
+ * 
+ */
 public class andreSel2 {
 
 	private WebDriver driver;
 	private String baseUrl = "";
 	private StringBuffer verificationErrors = new StringBuffer();
 	String tag = "andreSel2 inf: ";
+
+	// Ofen used Path for elements
+	private String waitForResult = "//div[@id='displayableResult']/div[@id='tables']";
+	private String okButton = "//div[@class='ui-dialog-buttonset']/button/span[text()='Ok']";// For
+																								// the
+																								// pupops
 
 	/**
 	 * 
@@ -30,17 +37,12 @@ public class andreSel2 {
 	 * 
 	 * * @param driver Webdriver for example FF IE
 	 */
-	public void runTests(WebDriver driver) {
-		try {
-			this.driver = driver;
-			if (driver != null) {
-				testAndreSel2();
-			}
-			System.out.println(tag + "Test was successful");
-		} catch (Exception e) {
-			System.out.println(tag + "Test failed");
-			System.out.println(e.toString());
+	public void runTests(WebDriver driver) throws Exception {
+		this.driver = driver;
+		if (driver != null) {
+			testAndreSel2();
 		}
+		System.out.println(tag + "Test was successful");
 
 	}
 
@@ -49,34 +51,33 @@ public class andreSel2 {
 	// driver = new FirefoxDriver();
 	// driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	// }
-
-	@Test
 	public void testAndreSel2() throws Exception {
 		System.out.println(tag + "Start test with: " + driver.toString());
 		driver.get(baseUrl);
-		driver.findElement(
-				By.cssSelector("#task_searchEvents > span.ui-button-text"))
-				.click();
+		waitXpath("//div[@id='task_searchEvents']");
+		driver.findElement(By.xpath("//div[@id='task_searchEvents']")).click();// ("#task_searchEvents > span.ui-button-text"))
+
+		// driver.findElement(By.xpath("//div[@id='task_searchEvents']")).click();////span[text()='Search
+		// Events']
+		// waitXpath("//h1[text()='Search Events']");
+		navigateTO("//div[@id='task_searchEvents']",
+				"//h1[text()='Search Events']");
+
 		// Open time selection
-		driver.findElement(
-				By.cssSelector("td > #time_button > span.ui-button-text"))
-				.click();
+		driver.findElement(By.id("time_button")).click();
 		driver.findElement(By.id("minDate1")).clear();
 		driver.findElement(By.id("minDate1")).sendKeys("2003-10-27T00:00:00");
 		driver.findElement(By.id("maxDate1")).clear();
 		driver.findElement(By.id("maxDate1")).sendKeys("2003-11-03T00:00:00");
-		driver.findElement(
-				By.xpath("//div[@class='ui-dialog-buttonset']/button")).click();
-		// Standard correction for the popups to press OK
+		driver.findElement(By.xpath(okButton)).click();
 
 		// Open event selection
 		driver.findElement(
 				By.cssSelector("#event_button > span.ui-button-text")).click();
 		driver.findElement(By.xpath(".//*[@id='input_table']/tbody/tr[15]/td"))
 				.click();
-		driver.findElement(
-				By.xpath("//div[@class='ui-dialog-buttonset']/button")).click();
-		waitXpath("//*[@id='voTables']/div[3]/h3", 0);
+		driver.findElement(By.xpath(okButton)).click();
+		waitXpath(waitForResult); // Default wait result is displayed.
 
 		// Sort the element in the List!
 		driver.findElement(
@@ -96,37 +97,39 @@ public class andreSel2 {
 		driver.findElement(By.id("minDate1")).sendKeys("2003-11-01T17:00:00");
 		driver.findElement(By.id("maxDate1")).clear();
 		driver.findElement(By.id("maxDate1")).sendKeys("2003-11-01T18:30:00");
-		driver.findElement(
-				By.xpath("//div[@class='ui-dialog-buttonset']/button[1]"))
-				.click();
+		driver.findElement(By.xpath(okButton)).click();
 		// Timselection is closed
 		navigateTO("//div[@id='task_searchInstCap']",
 				"//h1[text()='Search Instruments by Capability']");
 		driver.findElement(By.id("task_searchInstCap")).click();
 
-		// Drag & Drop
+		// Drag & Drop in Search Instruments by Capability
 		WebElement from = (WebElement) driver.findElement(By
 				.cssSelector("img.history_draggable.ui-draggable"));
 		WebElement to = (WebElement) driver.findElement(By
 				.xpath("//div[@class='resultDroppable ui-droppable']/img"));
 		(new Actions(driver)).dragAndDrop(from, to).perform();
-		waitXpath("//input[@name='HXR']",
-				1);
+		waitXpath(waitForResult);
+		// // HXR		//div[@id='ics_instrument']/table/tbody/tr/td/table/tbody/tr/td/input[@name='hxr']
+		//mabe select sxr to
 		driver.findElement(
-				By.xpath("//input[@name='HXR']"))
+				By.xpath("html/body/div[4]/div[3]/div[2]/div/div/div/div[3]/div/div[1]/div[2]/div/div/div[1]/table[2]/tbody/tr[8]/td/table/tbody/tr[1]/td[3]/input"))// //table[@class='advanced_param_table']/tbody/tr/td/input[@name='hxr']
 				.click();
+		try {	//Time needed to sort the table 
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println(tag + "WaitXpath error: thread.sleep");
+			e.printStackTrace();
+		}
 		driver.findElement(
-				By.xpath("//div/div/table[2]/tbody/tr[8]/td/table/tbody/tr/td[3]/input"))
-				.click();
-		driver.findElement(
-				By.xpath("//table[@id='resultTable0']/tbody/tr/td[3]")).click();
+				By.xpath("//table[@id='resultTable0']/tbody/tr/td[3][text()='RHESSI__HESSI_HXR']")).click();	//ict-instrument 
 		driver.findElement(
 				By.cssSelector("#response_save_selection > span.ui-button-text"))
 				.click();
-		driver.findElement(By.xpath("//button[@type='button']")).click();
+		driver.findElement(By.xpath(okButton)).click();
 
-		driver.findElement(
-				By.xpath("//div[@class='ui-dialog-buttonset']/button")).click();
+//		driver.findElement(
+//				By.xpath("//div[@class='ui-dialog-buttonset']/button")).click();
 
 		driver.findElement(
 				By.cssSelector("#task_searchData > span.ui-button-text"))
@@ -144,38 +147,32 @@ public class andreSel2 {
 	 * @param elementCase
 	 *            case 1: is Displayed. case 2: is Enabled. default 1.
 	 */
-	private void waitXpath(String path, int elementCase) {
-		for (int second = 0;; second++) {
-			if (second >= 20) {
-				System.out.println(tag + "Element cant be find " + path);
-			}
+	private void waitXpath(String path) {
+		// wait 60 seconds
+		for (int i = 0; i < 600; i++) {
+			assert i < 600; // only in dibug
 			try {
-				System.out.println(tag + "wai. element: "
-						+ driver.findElement(By.xpath(path)).toString()
-						+"; xpath: "+ path);
-				// /if (txt == null) {
-				if (elementCase == 0) {
-					if (driver.findElement(By.xpath(path)).isDisplayed()) {
-						break;
-					} else if (elementCase == 1) {
-						if (driver.findElement(By.xpath(path)).isEnabled()) {
-							break;
-						}
-					} else {
-						System.out.println(tag + "case don't exist.");
-					}
+				// System.out.println(tag + "wait. element: "
+				// + driver.findElement(By.xpath(path)).toString()
+				// + "; xpath: " + path);
+
+				if (driver.findElement(By.xpath(path)).isDisplayed()) {
+					return;
 				}
 
 			} catch (Exception e) {
 				// System.out.println(e.toString());
 			}
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
-				System.out.println(tag + "WaitXpath error");
+				System.out.println(tag + "WaitXpath error: thread.sleep");
 				e.printStackTrace();
 			}
+
 		}
+		System.out.println(tag + "Element cant be find " + path);
+		throw new RuntimeException();
 	}
 
 	/**
@@ -187,25 +184,32 @@ public class andreSel2 {
 	 *            Xpath of objekt to find.
 	 */
 	private void navigateTO(String path, String untilFound) {
+
 		for (int second = 0;; second++) {
-			if (second >= 20) {
-				System.out.println(tag + "Element cant be find " + path);
+			if (second >= 600) {
+				System.out.println(tag + "navigatTO: Element cant be find "
+						+ path);
+				assert second >= 600;
+				break;
 			}
 			try {
+
 				driver.findElement(By.xpath(path)).click();
+
 			} catch (Exception e) {
-				// System.out.println(e.toString());
+				// e.printStackTrace();
 			}
 			try {
 				if (driver.findElement(By.xpath(untilFound)).isDisplayed()) {
-					break;
+					return;
+					// break;
 				}
 			} catch (Exception e) {
-				
+				// e.printStackTrace();
 			}
 
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				System.out.println(tag + "navigateTO error");
 				e.printStackTrace();
@@ -213,7 +217,6 @@ public class andreSel2 {
 		}
 	}
 
-	@After
 	public void tearDown() throws Exception {
 
 		driver.quit();
