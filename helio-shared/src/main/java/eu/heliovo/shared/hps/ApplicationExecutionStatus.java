fcc245dae612d754d36c7c653e3389201eb2290a
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-10-06 14:27:12 +0000

    Sorting out problems with Marco's help.
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2339 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-shared/src/main/java/eu/heliovo/shared/hps/ApplicationExecutionStatus.java
@@ -0,0 +1,9 @@
+package eu.heliovo.shared.hps;
+
+public class ApplicationExecutionStatus 
+{
+	public static final String Running 		= "Running";
+	public static final String Completed 	= "Completed";
+	public static final String Failed 		= "Failed";
+	public static final String Undefined 	= "Undefined";
+}
