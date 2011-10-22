209bcd56abfe4108fde8e4e6d1d04c9801cf0a90
ddguevara <ddguevara@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-10-22 12:08:42 +0000

    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2397 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-frontend/helio-portal/grails-app/services/ch/i4ds/helio/DataQueryService.groovy
+++ helio-frontend/helio-portal/grails-app/services/ch/i4ds/helio/DataQueryService.groovy
@@ -3,6 +3,15 @@ import java.util.logging.Logger;
 
 import eu.heliovo.clientapi.frontend.ResultVT;
 import eu.heliovo.clientapi.frontend.SimpleInterface;
+import eu.heliovo.clientapi.linkprovider.*;
+import eu.heliovo.clientapi.HelioClient;
+import eu.heliovo.clientapi.query.*;
+import eu.heliovo.clientapi.query.asyncquery.*;
+import eu.heliovo.registryclient.*;
+import eu.heliovo.registryclient.HelioServiceName;
+ import eu.heliovo.shared.util.*;
+
+
 
 class DataQueryService {
 
@@ -20,19 +29,48 @@ class DataQueryService {
     	int startindex = 0;
     	
     	if(minDate.size()==0){
-    		log.info("queryService :: cant complete, date fields empty" );
-    		return;
+            log.info("queryService :: cant complete, date fields empty" );
+            return;
     	}
     	if(maxDate.size()==0){
-    		log.info("queryService :: cant complete, date fields empty" );
-    		return;
+            log.info("queryService :: cant complete, date fields empty" );
+            return;
     	}
-    	
-    	ResultVT resvt = SimpleInterface.queryService(serviceName, minDate, maxDate, from, where);
+        def helioClient = new HelioClient();
+        AsyncQueryService service;
+        HelioQueryResult result;
+        
+        switch (serviceName){
+            case "ICS":
+            service = (AsyncQueryService)helioClient.getServiceInstance(HelioServiceName.ICS, ServiceCapability.ASYNC_QUERY_SERVICE, "ivo://helio-vo.eu/ics/ics_pat");
+            break;
+            default:
+            HelioServiceName helioServiceName = HelioServiceName.valueOf(serviceName.toUpperCase());
+            service = (AsyncQueryService)helioClient.getServiceInstance(helioServiceName, ServiceCapability.ASYNC_QUERY_SERVICE, null);
+            break;
+
+        }
+        List<String>[] permuted = DateUtil.permuteLists(minDate,from)
+        minDate = permuted[0];
+        permuted = DateUtil.permuteLists(maxDate,from)
+        maxDate = permuted[0];
+        from  = permuted[1];
+
+         result = service.query(minDate, maxDate, from, where, 0, 0, null);
+         
+        
+        ResultVT resvt = new ResultVT(result.asVOTable(),result.getUserLogs());
+        //System.out.println(resultICS.asString());
+
+        //AsyncQueryService serviceICS= (AsyncQueryService)helioClient.getServiceInstance(HelioServiceName.DES, ServiceCapability.ASYNC_QUERY_SERVICE, null);
+        //HelioQueryResult resultICS = serviceICS.query(Arrays.asList("2007-07-10T12:00:00"), Arrays.asList("2007-07-11T12:00:00"), Arrays.asList("ACE"), "ACE.DERIV,V:/100.0:600.0", 0, 0, null);
+
+
+    	//ResultVT resvt = SimpleInterface.queryService(serviceName, minDate, maxDate, from, where);
     	
     	
     	log.info("queryService :Result:"+ resvt.getStringTable());
     	
     	return resvt;
     }
-}
+    }
