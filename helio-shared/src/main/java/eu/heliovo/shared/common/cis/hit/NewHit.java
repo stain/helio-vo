5537b4be64c2fa2220377244178c88038ae6fdef
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-12-01 14:23:14 +0000

    Redeployment of CIS
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2466 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

---
+++ helio-shared/src/main/java/eu/heliovo/shared/common/cis/hit/NewHit.java
@@ -0,0 +1,16 @@
+package eu.heliovo.shared.common.cis.hit;
+
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+
+import eu.heliovo.shared.common.cis.hit.info.HITInfo;
+
+public class NewHit 
+{
+	String	test	=	null;
+	
+	public NewHit() 
+	{
+		super();
+	}
+}
