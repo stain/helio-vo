1217b7544aec819cbe135d07de874c7c5613f0e4
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2011-12-08 09:46:37 +0000

    Adding preferences fields for HFE
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2512 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-shared/src/main/java/eu/heliovo/shared/common/cis/tags/HELIOTags.java
+++ helio-shared/src/main/java/eu/heliovo/shared/common/cis/tags/HELIOTags.java
@@ -3,14 +3,25 @@ package eu.heliovo.shared.common.cis.tags;
 public class HELIOTags 
 {
 	/*
-	 * Supported servicee
+	 * Supported services
 	 */
-	public static final String hec					=	"hec";
-	public static final String hec_value_1 			= 	"value_1";
-	public static final String hec_value_2 			= 	"value_2";
+	public static final String hfe									=	"hec";
+	public static final String hfe_field_1 							= 	"field_1";
+	public static final String hfe_field_2 							= 	"field_2";
+	public static final String hfe_field_1_std_value 				= 	"hfe_field_1_std_value";
+	public static final String hfe_field_2_std_value 				= 	"hfe_field_2_std_value";
 	
-	
-	public static final String dpas					=	"dpas";
-	public static final String dpas_data_providers 	= 	"data_providers";
-	public static final String dpas_value_2 		= 	"value_2";
+	public static final String hec									=	"hec";
+	public static final String hec_field_1 							= 	"field_1";
+	public static final String hec_field_2 							= 	"field_2";
+	public static final String hec_field_1_std_value 				= 	"hec_field_1_std_value";
+	public static final String hec_field_2_std_value 				= 	"hec_field_2_std_value";
+		
+	public static final String dpas									=	"dpas";
+	public static final String dpas_data_providers 					= 	"data_providers";
+	public static final String dpas_field_1 						= 	"field_1";
+	public static final String dpas_field_2 						= 	"field_2";
+	public static final String dpas_data_providers_std_value 		= 	"provider1, provider2, provider3, provider4";
+	public static final String dpas_field_1_std_value 				= 	"dpas_field_1_std_value";
+	public static final String dpas_field_2_std_value 				= 	"dpas_field_2_std_value";
 }
