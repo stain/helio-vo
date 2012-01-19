59089c4c369de1383b1a41269fc7e0724b1f1e36
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2012-01-19 19:16:00 +0000

    CIS GUI
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2603 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-shared/src/main/java/eu/heliovo/shared/common/cis/hit/NewHit.java
+++ helio-shared/src/main/java/eu/heliovo/shared/common/cis/hit/NewHit.java
@@ -1,9 +1,5 @@
 package eu.heliovo.shared.common.cis.hit;
 
-import org.ietf.jgss.GSSCredential;
-import org.ietf.jgss.GSSException;
-
-import eu.heliovo.shared.common.cis.hit.info.HITInfo;
 
 public class NewHit 
 {
