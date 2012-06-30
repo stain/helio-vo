112fb1b3f6edfc0b72b8cb8a29afd4b3541705a9
Gabmera <Gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2012-06-30 16:26:15 +0000

    Test for commit from new development machine
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2766 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-shared/src/main/java/eu/heliovo/shared/common/utilities/SystemUtilities.java
+++ helio-shared/src/main/java/eu/heliovo/shared/common/utilities/SystemUtilities.java
@@ -21,49 +21,6 @@ public class SystemUtilities
 		return sysExec("/bin/date");
 	}
 
-//	public String	execTest(String cmd)
-//	{
-//		String	result	=	"";
-//        try
-//        {            
-//            Runtime rt = Runtime.getRuntime();
-//            Process proc = rt.exec(cmd);
-//            /*
-//             * Error Stream
-//             */
-//            InputStream 		stderr = proc.getErrorStream();
-//            InputStreamReader 	isr = new InputStreamReader(stderr);
-//            BufferedReader 		br = new BufferedReader(isr);
-//			/*
-//			 * Output Stream
-//			 */
-//			InputStream inputstream = proc.getInputStream();
-//			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-//			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-//			/*
-//			 * Read the command output
-//			 */
-//            String line = null;
-//			while ((line = bufferedreader.readLine()) != null)
-//			{
-//				result	+=	line;
-//				result  += "\n";
-//			}
-//            
-//            System.out.println("");
-//            while ( (line = br.readLine()) != null)
-//            System.out.println(line);
-//            System.out.println("");
-//            int exitVal = proc.waitFor();
-//            System.out.println("Process exitValue: " + exitVal);
-//                      
-//        } catch (Throwable t)
-//          {
-//            t.printStackTrace();
-//          }
-//
-//		return null;
-//	}
 	
 	
 	public String sysExec(String command)
