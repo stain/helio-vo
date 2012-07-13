e410fca9f5c34b9337e92c1102a8bc3f293f7bae
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2012-07-13 13:31:58 +0000

    Added Grid role to the CIS.
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2787 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-shared/src/main/java/eu/heliovo/shared/cis/HELIORoles.java
+++ helio-shared/src/main/java/eu/heliovo/shared/cis/HELIORoles.java
@@ -5,14 +5,18 @@ import java.util.HashSet;
 public class HELIORoles 
 {
 	public 	static 	final	String simpleUser		=	"simpleUser";
+	public 	static 	final	String gridUser			=	"gridUser";
 	public 	static 	final	String administrator	=	"administrator";
 
 	private	static	HashSet<String>	roles			=	new HashSet<String>()
-			{private static final long serialVersionUID = 1L;
-			{
-				add(simpleUser);
-				add(administrator);
-			}};
+	{
+		private static final long serialVersionUID = 1L;
+		{
+			add(simpleUser);
+			add(administrator);
+			add(gridUser);
+		}
+	};
 	
 	public static	boolean	isValidRole(String role)
 	{
