c4593f7a362b39d710823cfb2b0d95ccd7b4c3db
gabmera <gabmera@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2012-07-19 15:32:00 +0000

    Various Utilities for security and serialization
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2810 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-shared/src/main/java/eu/heliovo/shared/cis/HELIOUsers.java
+++ helio-shared/src/main/java/eu/heliovo/shared/cis/HELIOUsers.java
@@ -4,4 +4,5 @@ public class HELIOUsers
 {
 	public static final String HelioStandardUser 			= "HelioStandardUser";
 	public static final String HelioDefaultAdministrator 	= "HelioAdministrator";
+	public static final String TestUser 					= "TestUser";
 }
