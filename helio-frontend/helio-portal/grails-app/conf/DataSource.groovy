aa900ac95f03ca1fb47da655b90b4d4d4338ab24
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2012-07-19 10:02:10 +0000

    update datasource config
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2808 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-frontend/helio-portal/grails-app/conf/DataSource.groovy
+++ helio-frontend/helio-portal/grails-app/conf/DataSource.groovy
@@ -3,6 +3,7 @@ dataSource {
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
+    dialect = org.hibernate.dialect.H2Dialect
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -11,41 +12,41 @@ hibernate {
 }
 // environment specific settings
 environments {
-	development {
-		dataSource {
-			dbCreate = "update" // one of 'create', 'create-drop','update'
-			url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop','update'
+            url = "jdbc:h2:~/.helio/helio-dev/portaldb;MVCC=TRUE"
             logSql = false
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-		}
-        
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+        }
+
         // disable cache for testing.
         hibernate {
             cache.use_second_level_cache=false
             cache.use_query_cache=false
             cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
         }
-	}
-	production {
-		dataSource {
-		    dbCreate = "update"
-            url = "jdbc:h2:prodHelioDb;MVCC=TRUE"
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:~/.helio/helio/portaldb;MVCC=TRUE"
             pooled = true
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+                maxActive = -1
+                minEvictableIdleTimeMillis=1800000
+                timeBetweenEvictionRunsMillis=1800000
+                numTestsPerEvictionRun=3
+                testOnBorrow=true
+                testWhileIdle=true
+                testOnReturn=true
+                validationQuery="SELECT 1"
             }
-		}
-	}
+        }
+    }
 }
