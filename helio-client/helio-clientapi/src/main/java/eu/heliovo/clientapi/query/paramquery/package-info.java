38cc1c4f8d6a215ad4b5e7d4b105fd7c95564624
mercuron <mercuron@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2012-08-21 12:57:01 +0000

    remove deprecated param query.
    git-svn-id: http://svn.code.sf.net/p/helio-vo/code/trunk@2835 5d9e9ddb-692e-4027-9be8-b4f4f3982da7

--- helio-client/helio-clientapi/src/main/java/eu/heliovo/clientapi/query/paramquery/package-info.java
+++
@@ -1,4 +0,0 @@
-package eu.heliovo.clientapi.query.paramquery;
-/**
- * Prototype implementation of some helper classes to hide PQL behind a query model. This is intended to be useful for scripting environments.
- */
